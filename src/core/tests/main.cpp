1940cbffefe976f221ac1619fb71a82a6d2e92c5
ybouret <yann.bouret@gmail.com>
2025-05-18 15:19:07 +0200

    wip

--- src/core/tests/main.cpp
+++ src/core/tests/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-#include "y/check/usual.hpp"
+#include "y/core/variadic.hpp"
 
 using namespace Yttrium;
 
 int main()
 {
-    assert(  Good(0,0) || Die("Invalid Buffer") );
-	return 0;
+    char buf[4];
+    memset(buf,0,sizeof(buf));
+    const int res = Core::Variadic::Sprintf(buf, sizeof(buf), "%s", "Hello");
+    std::cerr << buf << ", res=" << res << std::endl;
+    return 0;
 }
