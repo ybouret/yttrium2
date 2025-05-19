569d713e2c31d855c95fd8801c492afd85d93ad7
Yann Bouret <yann.bouret@unice.fr>
2025-05-19 12:47:50 +0200

    wip

--- src/core/y/core/text.hpp
+++ src/core/y/core/text.hpp
@@ -3,6 +3,7 @@
 #ifndef Y_Core_Text_Included
 #define Y_Core_Text_Included 1
 
+#include "y/system/platform.hpp"
 #include "y/system/types.hpp"
 
 
@@ -12,10 +13,10 @@ namespace Yttrium
     {
         struct Text
         {
-            static size_t      Length(const char * const text) noexcept;
-            static char *      Copy(char * const       outputBuffer,
-                                    const size_t       outputLength,
-                                    const char * const text) noexcept;
+            static size_t  Length(const char * const text) noexcept;
+            static char *  Copy(char * const       outputBuffer,
+                                const size_t       outputLength,
+                                const char * const text) noexcept;
         };
     }
 
