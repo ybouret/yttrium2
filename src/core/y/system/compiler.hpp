251b205f98312320dbfb4f71ca547997454636fc
Yann Bouret <yann.bouret@unice.fr>
2025-05-19 09:23:41 +0200

    wip

--- src/core/y/system/compiler.hpp
+++ src/core/y/system/compiler.hpp
@@ -8,32 +8,35 @@
 // detect clang
 
 #if defined(__clang__)
-#define Y_Compiler "Clang"
-#define Y_CLANG 1
+#  define Y_Compiler "Clang"
+#  define Y_CLANG 1
 #endif
 
 // detect Intel
 #if defined(__INTEL_COMPILER)
-#define Y_Compiler "Intel"
-#define Y_ICC 1
-#define noexcept throw()
+#  define Y_Compiler "Intel"
+#  define Y_ICC 1
+#  define noexcept throw()
 #endif
 
 #if defined(_MSC_VER)
-#define Y_Compiler "Microsoft"
-#define Y_MSC 1
+#  define Y_Compiler "Microsoft"
+#  define Y_MSC 1
 #endif
 
 // detect GNU
 #if !defined(Y_Compiler) && defined(__GNUC__)
-#define Y_Compiler "GNU"
-#define Y_GNU 1
+#  define Y_Compiler "GNU"
+#  define Y_GNU 1
+#  if(__GNUC__<7)
+#  define noexcept throw()
+#  endif
 #endif
 
 
 
 #if !defined(Y_Compiler)
-#error "Unknown Compiler"
+#  error "Unknown Compiler"
 #endif
 
 #endif
