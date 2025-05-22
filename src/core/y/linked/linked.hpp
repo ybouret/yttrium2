a413b785d7f7866785d6a3d72947d2d9799c4d9c
ybouret <yann.bouret@gmail.com>
2025-05-22 13:44:59 +0200

    wip

---
+++ src/core/y/linked/linked.hpp
@@ -0,0 +1,24 @@
+//! \file
+
+#ifndef Y_Linked_Included
+#define Y_Linked_Included 1
+
+#include "y/check/usual.hpp"
+
+namespace Yttrium
+{
+    namespace Kernel
+    {
+        template <typename NODE>
+        class Linked
+        {
+        public:
+
+        private:
+            
+        };
+    }
+}
+
+#endif
+
