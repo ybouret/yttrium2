611b7b216b5542046a9678b3cb7c79c7e92b3335
ybouret <yann.bouret@gmail.com>
2025-05-27 14:06:58 +0200

    wip

--- src/core/y/system/pid.hpp
+++ src/core/y/system/pid.hpp
@@ -9,8 +9,15 @@ namespace Yttrium
 {
     namespace System
     {
+        //! System Process Identifier
         struct ProcessID
         {
+            //! get the id
+            /**
+             - BSD: pid_t   getpid()
+             - WIN: DWORD ::GetProcessId()
+             \return process-id put in uint64_t
+             */
             static uint64_t Get();
         };
     }
