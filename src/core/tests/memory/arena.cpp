1d308a789a3b1fc84c2a91af7dfd46126bc7a47c
ybouret <yann.bouret@gmail.com>
2025-05-27 20:59:38 +0200

    wip

--- src/core/tests/memory/arena.cpp
+++ src/core/tests/memory/arena.cpp
@@ -4,6 +4,7 @@
 #include "y/system/rand.hpp"
 #include "y/utest/run.hpp"
 #include "y/check/usual.hpp"
+#include "y/memory/object/book.hpp"
 
 #include <cstring>
 
@@ -37,28 +38,29 @@ Y_UTEST(memory_arena)
 	System::Rand ran;
 	Y_SIZEOF(Memory::Arena);
 
+    void* addr[MaxSize];
+    size_t size = 0;
+    Y_Memory_BZero(addr);
 
-	size_t blockSize = 100;
-	size_t pageBytes = 0;
+    for(size_t blockSize=1;blockSize<=16;++blockSize)
+    {
+        std::cerr << "[blockSize=" << blockSize << "]" << std::endl;
+        for(size_t pageBytes = 128; pageBytes <= 8192; pageBytes <<= 1)
+        {
+            Memory::Arena arena(blockSize, pageBytes);
 
-	if (argc > 1) blockSize = size_t(atol(argv[1]));
-	if (argc > 2) pageBytes = size_t(atol(argv[2]));
+            fill(addr, size, arena);
+            for (int iter = 0; iter < 100; ++iter)
+            {
+                empty(size >> 1, addr, size, arena, ran);
+                fill(addr, size, arena);
+            }
+            empty(0, addr, size, arena, ran);
+        }
+    }
 
-	Memory::Arena arena(blockSize, pageBytes);
+    Memory::Book::Location().display(std::cerr);
 
-
-
-	void* addr[MaxSize];
-	size_t size = 0;
-	Y_Memory_BZero(addr);
-
-	fill(addr, size, arena);
-	for (int iter = 0; iter < 100; ++iter)
-	{
-		empty(size >> 1, addr, size, arena, ran);
-		fill(addr, size, arena);
-	}
-	empty(0, addr, size, arena, ran);
 
 }
 Y_UDONE()
