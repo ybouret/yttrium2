09dcedf19e852e6c12317353d3d1333972621aec
ybouret <yann.bouret@gmail.com>
2025-05-28 09:48:57 +0200

    wip

--- src/y/z/configure.cmake
+++ src/y/z/configure.cmake
@@ -4,8 +4,16 @@ set(ZLIB_ROOT    "zlib-${ZLIB_VERSION}")
 set(ZLIB_TGZ     "${ZLIB_ROOT}.tar.gz")
 set(ZLIB_URL     "https://www.zlib.net/${ZLIB_TGZ}")
 
+set(ZLIB_PATH "${CMAKE_CURRENT_SOURCE_DIR}/${ZLIB_ROOT}")
 if (NOT EXISTS "${ZLIB_TGZ}")
+    message( STATUS "downloading ${ZLIB_URL}")
     file( DOWNLOAD "${ZLIB_URL}" "${ZLIB_TGZ}"
     SHOW_PROGRESS
     EXPECTED_HASH SHA256=9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23)
+endif()
+
+if(NOT EXISTS "${ZLIB_PATH}")
+    message(STATUS "extracting ${ZLIB_ROOT}")
+    #file( ARCHIVE_EXTRACT INPUT "${ZLIB_TGZ}" LIST_ONLY)
+    file( ARCHIVE_EXTRACT INPUT "${ZLIB_TGZ}" DESTINATION ${CMAKE_CURRENT_SOURCE_DIR})
 endif()
