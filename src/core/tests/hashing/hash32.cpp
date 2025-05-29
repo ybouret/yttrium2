2b3ac47032f857fe2468c8f9a646d877e9de25e7
ybouret <yann.bouret@gmail.com>
2025-05-29 20:43:47 +0200

    wip

--- src/core/tests/hashing/hash32.cpp
+++ src/core/tests/hashing/hash32.cpp
@@ -2,6 +2,7 @@
 #include "y/check/crc32.hpp"
 
 #include "y/core/linked/list/cxx.hpp"
+#include "y/core/utils.hpp"
 #include "y/utest/run.hpp"
 
 using namespace Yttrium;
@@ -57,6 +58,8 @@ namespace
         Y_Disable_Assign(Node);
     };
 
+    static bool verbosity = true;
+
     template <
     unsigned p,
     unsigned q,
@@ -64,14 +67,15 @@ namespace
     struct TestHash32
     {
         template <typename HASH>
-        static inline void With(HASH &hash)
+        static inline size_t With(HASH &hash)
         {
             static const size_t _1        = 1;
             static const size_t TableSize = _1 << p;
             static const size_t TableMask = TableSize-1;
             static const size_t ItemCount = _1 << q;
 
-            std::cerr << "Setting " << ItemCount << "/" << delta << " in " << TableSize << " slots" << std::endl;
+            if(verbosity) std::cerr << "Setting " << ItemCount << "/" << delta << " in " << TableSize << " slots" << std::endl;
+
             Node::List Table[TableSize];
 
             for(size_t i=delta;i<=ItemCount;i += delta)
@@ -80,19 +84,24 @@ namespace
                 Table[ hash(node->indx) & TableMask ].pushTail(node);
             }
 
+            size_t maxLoad = Table[0].size;
             for(size_t i=0;i<TableSize;++i)
             {
                 Node::List &slot = Table[i];
-                slot.sort(Node::Compare);
-                std::cerr << "[" << std::setw(4) << i << "]";
-                std::cerr << "#" << std::setw(5) << slot.size << " |";
-                for(const Node *node=slot.head;node;node=node->next)
+                if(verbosity)
                 {
-                    std::cerr << " " << node->indx;
+                    slot.sort(Node::Compare);
+                    std::cerr << "[" << std::setw(4) << i << "]";
+                    std::cerr << "#" << std::setw(5) << slot.size << " |";
+                    for(const Node *node=slot.head;node;node=node->next)
+                    {
+                        std::cerr << " " << node->indx;
+                    }
+                    std::cerr << std::endl;
                 }
-                std::cerr << std::endl;
+                InSituMax(maxLoad, slot.size);
             }
-
+            return maxLoad;
         }
     };
 
@@ -101,9 +110,13 @@ namespace
 
     static inline size_t UsingCRC32(const size_t x)
     {
+#if 0
         const uint16_t xx = uint16_t(x);
         const size_t  ans = CRC32:: Run(xx,seed32);
         return ans;
+#else
+        return CRC32::Run(x,seed32);
+#endif
     }
 
 
@@ -112,6 +125,11 @@ namespace
 
 Y_UTEST(hashing_hash32)
 {
-    TestHash32<5,9,1>::With(UsingCRC32);
+    //verbosity = false;
+    for(seed32=0;seed32<1;++seed32)
+    {
+        const size_t maxLoad = TestHash32<5,10,1>::With(UsingCRC32);
+        std::cerr << "=> maxLoad = " << maxLoad << std::endl;
+    }
 }
 Y_UDONE()
