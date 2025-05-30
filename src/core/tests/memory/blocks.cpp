efa77ac7b152ff49c4848feb29c7ba2ffb431ddd
Yann Bouret <yann.bouret@unice.fr>
2025-05-30 09:19:00 +0200

    wip

--- src/core/tests/memory/blocks.cpp
+++ src/core/tests/memory/blocks.cpp
@@ -18,12 +18,13 @@ namespace
         size_t size;
     };
 
-    static const size_t MaxBlocks = 1000;
+    static const size_t MaxBlocks = 10000;
     static Block  blk[MaxBlocks];
     static size_t num = 0;
 
     void fill(  Memory::Object::Blocks &blocks, System::Rand &ran )
     {
+        (std::cerr << '+').flush();
         while(num<MaxBlocks)
         {
             Block &b = blk[num];
@@ -35,6 +36,7 @@ namespace
 
     void empty(const size_t to, Memory::Object::Blocks &blocks, System::Rand &ran)
     {
+        (std::cerr << '-').flush();
         ran.shuffle(blk,num);
         while(num>to)
         {
@@ -58,10 +60,11 @@ Y_UTEST(memory_blocks)
     void * p = blocks.acquire(10); std::cerr << "p@" << p << std::endl;
     blocks.release(p,10);
 #endif
-    
+
 #if 1
     Y_Memory_BZero(blk);
 
+    std::cerr << '[';
     fill(blocks,ran);
     for(size_t iter=0;iter<10;++iter)
     {
@@ -69,6 +72,7 @@ Y_UTEST(memory_blocks)
         fill(blocks,ran);
     }
     empty(0,blocks,ran);
+    std::cerr << ']' << std::endl;
 #endif
 
 }
