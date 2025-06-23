e81719cae95f1e8c61fdece00ba7d2d38b6780f8
ybouret <yann.bouret@gmail.com>
2025-06-23 10:35:14 +0200

    wip

---
+++ src/core/y/concurrent/data/inventory.hpp
@@ -0,0 +1,15 @@
+
+//! \file
+
+#ifndef Y_Concurrent_Inventory_Included
+#define Y_Concurrent_Inventory_Included 1
+
+#include "y/protean/coop/heavy/list.hpp"
+
+namespace Yttrium
+{
+    
+}
+
+#endif
+
