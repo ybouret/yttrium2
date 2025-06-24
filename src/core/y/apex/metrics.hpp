fb47bf9aefef94988827a1551fe4485b498f9ae3
ybouret <yann.bouret@gmail.com>
2025-06-24 10:42:11 +0200

    wip

---
+++ src/core/y/apex/metrics.hpp
@@ -0,0 +1,8 @@
+
+//! \file
+
+#ifndef Y_Apex_Metrics_Included
+#define Y_Apex_Metrics_Included 1
+
+#endif
+
---
+++ src/core/y/apex/types.hpp
@@ -0,0 +1,8 @@
+
+//! \file
+
+#ifndef Y_Apex_Types_Included
+#define Y_Apex_Types_Included 1
+
+#endif
+
