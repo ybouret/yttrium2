ee3cabde25be37c9114651261d890a9b1cb98d79
Yann Bouret <yann.bouret@unice.fr>
2025-06-25 13:23:27 +0200

    wip

--- src/core/tests/apex/block.cpp
+++ src/core/tests/apex/block.cpp
@@ -5,8 +5,10 @@
 #include "y/object.hpp"
 #include "y/apex/metrics.hpp"
 #include "y/calculus/bits-for.hpp"
+#include "y/type/sign.hpp"
 
 #include <iostream>
+#include <iomanip>
 #include <cstring>
 
 namespace Yttrium
@@ -54,6 +56,10 @@ if ( !(EXPR) ) { std::cerr << #EXPR << " failure" << std::endl; return false; } 
                 return doCheck();
             }
 
+            friend std::ostream & operator<<(std::ostream &os, const BlockAPI &self)
+            {
+                return self.print(os);
+            }
 
             virtual size_t update() noexcept = 0;
 
@@ -71,6 +77,8 @@ if ( !(EXPR) ) { std::cerr << #EXPR << " failure" << std::endl; return false; } 
             Y_Disable_Copy_And_Assign(BlockAPI);
             virtual bool doCheck() const noexcept = 0;
             virtual void zeroPad()       noexcept = 0;
+            virtual std::ostream & print(std::ostream&) const = 0;
+
         };
 
         const ViewType BlockAPI::VTable[] =
@@ -108,11 +116,17 @@ if ( !(EXPR) ) { std::cerr << #EXPR << " failure" << std::endl; return false; } 
                 return msw * UnitBits + Calculus::BitsFor::Count(data[msw]);
             }
 
-            virtual size_t update() noexcept
+
+            inline  void adjust() noexcept
             {
                 while( (size>0) && (0 == data[size-1]) )
                     --size;
                 assert(isValid());
+            }
+
+            virtual size_t update() noexcept
+            {
+                adjust();
                 return bits();
             }
 
@@ -128,10 +142,99 @@ if ( !(EXPR) ) { std::cerr << #EXPR << " failure" << std::endl; return false; } 
             inline virtual bool doCheck() const noexcept
             {
                 assert(size<=maxi);
-                if(size>0) Y_Block_Check(0==data[size-1]);
+                if(size>0) Y_Block_Check(0!=data[size-1]);
                 for(size_t i=size;i<maxi;++i) Y_Block_Check(0==data[i]);
                 return true;
             }
+
+            inline virtual std::ostream & print(std::ostream &os) const
+            {
+                os << "@." << std::setw(2) << UnitBits << '[';
+                size_t i=size;
+                while(i-- > 0)
+                    os << ' ' << Hexadecimal(data[i]);
+                os << ' ' << ']' << '#' << size;
+                return os;
+            }
+        };
+
+        struct Transmogrify
+        {
+            template <typename SMALL, typename LARGE> static inline
+            void Expand(Block<SMALL> &target, const Block<LARGE> &source) noexcept
+            {
+                assert(source.isValid());
+                static const unsigned RHO = sizeof(LARGE)/sizeof(SMALL);
+                static const unsigned SHR = sizeof(SMALL) * 8;
+                SMALL *      small = target.data;
+                const LARGE *large = source.data;
+                for(size_t i=source.size;i>0;--i)
+                {
+                    const LARGE value = *(large++);
+                    for(unsigned j=0;j<RHO;++j)
+                    {
+                        *(small++) = SMALL(value>>(j*SHR));
+                    }
+                }
+                target.size = source.size * RHO;
+                target.adjust();
+            }
+
+            template <typename LARGE, typename SMALL> static inline
+            void Shrink(Block<LARGE> &target, const Block<SMALL> &source) noexcept
+            {
+                assert(source.isValid());
+                static const unsigned RHO = sizeof(LARGE)/sizeof(SMALL);
+                static const unsigned SHL = sizeof(SMALL) * 8;
+
+                const size_t words = source.size;
+                const SMALL *small = source.data;
+                LARGE       *large = target.data;
+                for(size_t i = (target.size = Alignment::To<LARGE>::Ceil(words*sizeof(SMALL))/sizeof(LARGE)), used=0;i>0;--i)
+                {
+                    LARGE value = *(small++); ++used;
+                    for(unsigned j=1;j<RHO;++j)
+                    {
+                        const LARGE temp = *(small++);
+                        value |= (temp << (j*SHL));
+                        if(++used>=words) break;
+                    }
+                    *(large++) = value;
+                    if(used>=words) break;
+                }
+                assert(target.isValid());
+            }
+
+
+            template <typename SMALL, typename LARGE> static inline
+            void To(Block<SMALL> &target, const Block<LARGE> &source, const IntToType<Negative> &) noexcept
+            {
+                Expand(target,source);
+            }
+
+            template <typename LARGE, typename SMALL> static inline
+            void To(Block<LARGE> &target, const Block<SMALL> &source, const IntToType<Positive> &) noexcept
+            {
+                Shrink(target,source);
+            }
+
+            template <typename T> static inline
+            void To(Block<T> &, const Block<T> &, const IntToType<__Zero__> &) noexcept
+            {
+
+            }
+
+            template <typename TARGET, typename SOURCE> static inline
+            void To(Block<TARGET> &target, const Block<SOURCE> &source) noexcept
+            {
+                static const unsigned         TSize = sizeof(TARGET);
+                static const unsigned         SSize = sizeof(SOURCE);
+                static const SignType         Which = (TSize < SSize) ? Negative : (SSize<TSize ? Positive : __Zero__);
+                static const IntToType<Which> choice = {};
+                To(target,source,choice);
+            }
+
+
         };
 
         class Blocks
@@ -156,7 +259,7 @@ if ( !(EXPR) ) { std::cerr << #EXPR << " failure" << std::endl; return false; } 
             }
 
             template <typename T> inline
-            Block<T>  & block() noexcept { return * static_cast<Block<T> *>( &base[BlockProtoSize*IntegerLog2For<T>::Value]); }
+            Block<T>  & block() noexcept { return *(Block<T> *) &base[BlockProtoSize*IntegerLog2For<T>::Value]; }
 
             virtual ~Blocks() noexcept
             {
@@ -173,11 +276,6 @@ if ( !(EXPR) ) { std::cerr << #EXPR << " failure" << std::endl; return false; } 
             size_t          dataBytes;
             void *   const  dataEntry;
 
-            template <typename T> inline
-            void * getBlockAddr() const noexcept {
-                return &base[BlockProtoSize*IntegerLog2For<T>::Value];
-            }
-
 
             static void * Query(const unsigned shift)
             {
@@ -190,276 +288,7 @@ if ( !(EXPR) ) { std::cerr << #EXPR << " failure" << std::endl; return false; } 
 
 
 
-#if 0
-        template <size_t LN2> struct UnsignedForLn2;
-        template <> struct UnsignedForLn2<0> { typedef uint8_t  Type; };
-        template <> struct UnsignedForLn2<1> { typedef uint16_t Type; };
-        template <> struct UnsignedForLn2<2> { typedef uint32_t Type; };
-        template <> struct UnsignedForLn2<3> { typedef uint64_t Type; };
-
-
-
-
-#define Y_Block_Check(EXPR) do { \
-if ( !(EXPR) ) { std::cerr << #EXPR << " failure" << std::endl; return false; } \
-} while(false)
-
-        class BlockAPI
-        {
-        public:
-            explicit BlockAPI(const size_t n) :
-            size(0),
-            capacity(n)
-            {
-            }
-
-            virtual ~BlockAPI() noexcept
-            {
-
-            }
-
-            friend std::ostream & operator<<(std::ostream &os, const BlockAPI &self)
-            {
-                return self.print(os);
-            }
-
-            virtual void update()  noexcept = 0;
-            virtual void upgrade() noexcept = 0;
-
-            bool isValid() const noexcept
-            {
-                Y_Block_Check(size<=capacity);
-                return checkData();
-            }
-
-            size_t       size;
-            const size_t capacity;
-
-        private:
-            Y_Disable_Copy_And_Assign(BlockAPI);
-            virtual bool            checkData()  const noexcept = 0;
-            virtual std::ostream &  print(std::ostream &) const = 0;
-        };
-
-        template <typename T>
-        class Block : public BlockAPI
-        {
-        public:
-            inline explicit Block(void * const entry,
-                                  const size_t count) noexcept :
-            BlockAPI(count),
-            data( static_cast<T*>(entry) )
-            {
-                assert(capacity>0);
-                assert(entry!=0);
-                assert(Memory::Stealth::Are0(data,capacity*sizeof(T)));
-            }
-
-            inline virtual ~Block() noexcept
-            {
-            }
-
-            virtual void upgrade() noexcept
-            {
-                assert(size<=capacity);
-                for(size_t i=size;i<capacity;++i) data[i] = 0;
-                update();
-            }
-
-            virtual void update() noexcept
-            {
-                assert(size<=capacity);
-                while(size>0 && 0 == data[size-1]) --size;
-                assert(isValid());
-            }
-
-
-            T * const data;
-
-        private:
-            Y_Disable_Copy_And_Assign(Block);
-            virtual bool checkData() const noexcept
-            {
-                Y_Block_Check(data!=0);
-                if(size>0) { Y_Block_Check(data[size-1]!=0); }
-                Y_Block_Check(Memory::Stealth::Are0(data+size,(capacity-size)*sizeof(T)));
-                return true;
-            }
-
-            virtual std::ostream & print(std::ostream &os) const
-            {
-                os << '[';
-                size_t i=size;
-                while(i-- > 0)
-                    os << ' ' << Hexadecimal(data[i]);
-                //return Hexadecimal::Display(os,data,size);
-
-                return os << ' ' <<  ']';
-            }
-
-        };
-
-        struct Convert
-        {
-            template <typename SMALL, typename LARGE> static inline
-            void Expand(Block<SMALL> &target, const Block<LARGE> &source)
-            {
-                assert( source.isValid() );
-                assert( target.isValid() );
-
-                static const unsigned shr  = sizeof(SMALL) << 3;
-                static const unsigned loop = sizeof(LARGE)/sizeof(SMALL);
-                SMALL *       tgt = target.data;
-                const LARGE * src = source.data;
-                for(size_t i=source.size;i>0;--i)
-                {
-                    const LARGE value = *(src++);
-                    for(size_t j=0;j<loop;++j)
-                        *(tgt++) = SMALL( value >> (shr*j) );
-                }
-                target.size = source.size * loop;
-                target.upgrade();
-            }
-
-            template <typename LARGE, typename SMALL> static inline
-            void Shrink(Block<LARGE> &target, const Block<SMALL> &source)
-            {
-                assert( source.isValid() );
-                assert( target.isValid() );
-                static const unsigned shl  = sizeof(SMALL) << 3;
-                static const unsigned loop = sizeof(LARGE)/sizeof(SMALL);
-                LARGE *       tgt = target.data;
-                const SMALL * src = source.data;
-                const size_t  size = source.size;
-                for(size_t i= (target.size = Alignment::To<LARGE>::Ceil(size)/sizeof(LARGE)),used=0;i>0;--i)
-                {
-                    LARGE value = *(src++); ++used;
-                    for(size_t j=1;j<loop;++j)
-                    {
-                        const LARGE temp = *(src++); ++used;
-                        value |= (temp << (j*shl));
-                        if(used>=size) break;
-                    }
-                    *(tgt++) = value;
-                    if(used>=size) break;
-                }
-                target.upgrade();
-            }
-
-            template <typename LHS, typename RHS> static inline
-            void To(Block<LHS> &lhs, const Block<RHS> &rhs) noexcept
-            {
-                static const IntToType< sizeof(LHS)< sizeof(RHS) > choice = {};
-                To(lhs,rhs,choice);
-            }
-
-            template <typename LHS, typename RHS> static inline
-            void To(Block<LHS> &lhs, const Block<RHS> &rhs, const IntToType<true> &) noexcept
-            {
-                Expand(lhs,rhs);
-            }
-
-            template <typename LHS, typename RHS> static inline
-            void To(Block<LHS> &lhs, const Block<RHS> &rhs, const IntToType<false> &) noexcept
-            {
-                Shrink(lhs,rhs);
-            }
-
-
-        };
-
-
-
-
-        class Blocks
-        {
-        public:
-            typedef Block<uint8_t> BlockProto;
-            static const size_t    BlockProtoSize   = sizeof(BlockProto);
-            static const size_t    NumBlocks        = Metrics::WordShift + 1;
-            static const size_t    MinBytesPerBlock = Metrics::WordBytes;
-
-
-            explicit Blocks(size_t bytesPerBlock) :
-            base(0),
-            wksp(),
-            shift(0),
-            count( Metrics::BytesFor( MaxOf(bytesPerBlock,MinBytesPerBlock) * NumBlocks,shift) ),
-            entry( Query(shift) )
-            {
-                assert(count/NumBlocks>=bytesPerBlock);
-                bytesPerBlock = count/NumBlocks;
-                Coerce(base) = static_cast<uint8_t *>(Y_Memory_BZero(wksp));
-                {
-                    uint8_t * p = entry;
-                    new ( addrFor<uint8_t>() )  Block<uint8_t>(p,bytesPerBlock);                   p+=bytesPerBlock;
-                    new ( addrFor<uint16_t>() ) Block<uint16_t>(p,bytesPerBlock/sizeof(uint16_t)); p+=bytesPerBlock;
-                    new ( addrFor<uint32_t>() ) Block<uint32_t>(p,bytesPerBlock/sizeof(uint32_t)); p+=bytesPerBlock;
-                    new ( addrFor<uint64_t>() ) Block<uint64_t>(p,bytesPerBlock/sizeof(uint64_t)); p+=bytesPerBlock;
-                }
-            }
-
-            template <typename T> inline
-            Block<T> & block() noexcept
-            {
-                return *static_cast<Block<T> *>( addrFor<T>() );
-            }
-
-            template <typename T> inline
-            const Block<T> & block() const noexcept
-            {
-                return *static_cast<const Block<T> *>( addrFor<T>() );
-            }
-
-            virtual ~Blocks() noexcept
-            {
-                static Archon &archon = Archon::Location();
-                archon.store(shift,entry);
-            }
-
-        private:
-            Y_Disable_Copy_And_Assign(Blocks);
-            uint8_t * const base;
-            void *          wksp[ Alignment::WordsGEQ< NumBlocks * sizeof(Block<uint8_t>) >::Count ];
-            unsigned        shift;
-            const size_t    count;
-            uint8_t * const entry;
-
-            template <typename T> inline
-            void * addrFor() const noexcept
-            {
-                return (void*)( base + IntegerLog2For<T>::Value * BlockProtoSize);
-            }
-
-            static  uint8_t * Query(const unsigned userShift)
-            {
-                static Archon &archon = Archon::Instance();
-                return static_cast<uint8_t *>( archon.query(userShift) );
-            }
-        };
-
-        class Model : public Object, public Blocks
-        {
-        public:
-            explicit Model(const size_t bytesPerBlock) :
-            Object(),
-            Blocks( bytesPerBlock ),
-            bytes( block<uint8_t>().size ),
-            bits(0)
-            {
-            }
-
-            virtual ~Model() noexcept {}
-
-            const size_t & bytes;
-            size_t         bits;
-
-        private:
-            Y_Disable_Copy_And_Assign(Model);
-
-        };
-#endif
-
+#
     }
 }
 
@@ -521,7 +350,61 @@ Y_UTEST(apex_block)
     Y_PRINTV( Apex::Blocks::BlockProtoSize );
     Y_SIZEOF( Apex::Blocks );
     {
+        const uint64_t p64[2] = { 0xabdc, 0x1234 };
         Apex::Blocks b(0);
+
+        Apex::Block<uint64_t> &b64 = b.block<uint64_t>();
+        b64.data[0] = p64[0];
+        b64.data[1] = p64[1];
+        b64.size    = 2;
+        Y_ASSERT(b64.isValid());
+
+        Apex::Block<uint32_t> &b32 = b.block<uint32_t>();
+        Apex::Block<uint16_t> &b16 = b.block<uint16_t>();
+        Apex::Block<uint8_t>  &b8  = b.block<uint8_t>();
+
+        std::cerr << "I/O with 64 bits" << std::endl;
+        std::cerr << b64 << std::endl;
+        Apex::Transmogrify::To(b32,b64);
+        std::cerr << b32 << std::endl;
+        Apex::Transmogrify::To(b64,b32);
+        std::cerr << b64 << std::endl; Y_ASSERT( 0 == memcmp(b64.data,p64,sizeof(p64)));
+
+        Apex::Transmogrify::To(b16,b64);
+        std::cerr << b16 << std::endl;
+        Apex::Transmogrify::To(b64,b16);
+        std::cerr << b64 << std::endl; Y_ASSERT( 0 == memcmp(b64.data,p64,sizeof(p64)));
+
+        Apex::Transmogrify::To(b8,b64);
+        std::cerr << b8 << std::endl;
+        Apex::Transmogrify::To(b64,b8);
+        std::cerr << b64 << std::endl; Y_ASSERT( 0 == memcmp(b64.data,p64,sizeof(p64)));
+
+        std::cerr << "I/O with 32 bits" << std::endl;
+        Apex::Transmogrify::To(b32,b64);
+        uint32_t p32[4] = { 0 };
+        memcpy(p32,b32.data,sizeof(p32));
+        Apex::Transmogrify::To(b16,b32);
+        std::cerr << b16 << std::endl;
+        Apex::Transmogrify::To(b32,b16);
+        std::cerr << b32 << std::endl; Y_ASSERT( 0 == memcmp(b32.data,p32,sizeof(p32)));
+
+        Apex::Transmogrify::To(b8,b32);
+        std::cerr << b8 << std::endl;
+        Apex::Transmogrify::To(b32,b8);
+        std::cerr << b32 << std::endl; Y_ASSERT( 0 == memcmp(b32.data,p32,sizeof(p32)));
+
+
+        std::cerr << "I/O with 16 bits" << std::endl;
+        Apex::Transmogrify::To(b16,b32);
+        uint16_t p16[8] = { 0 };
+        memcpy(p16,b16.data,sizeof(p16));
+        Apex::Transmogrify::To(b8,b16);
+        std::cerr << b8 << std::endl;
+        Apex::Transmogrify::To(b16,b8);
+        std::cerr << b16 << std::endl; Y_ASSERT( 0 == memcmp(b16.data,p16,sizeof(p16)));
+
+
     }
 
 }
