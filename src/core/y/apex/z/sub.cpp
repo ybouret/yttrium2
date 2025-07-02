a5777c96c3e150bee9dfbabe661f62f9dc6bdd94
ybouret <yann.bouret@gmail.com>
2025-07-02 13:41:01 +0200

    wip

--- src/core/y/apex/z/sub.cpp
+++ src/core/y/apex/z/sub.cpp
@@ -22,8 +22,25 @@ namespace Yttrium
 
                 case Sign::PN: { const Natural sum = lhs.n + rhs.n; return Integer(Positive,sum); }
                 case Sign::NP: { const Natural sum = lhs.n + rhs.n; return Integer(Negative,sum); }
-            }
 
+                case Sign::PP:
+                    switch( Natural::Compare(lhs.n,rhs.n) )
+                    {
+                        case __Zero__: break; // ==> zero
+                        case Positive: { const Natural dif = lhs.n-rhs.n; return Integer(Positive,dif); }
+                        case Negative: { const Natural dif = rhs.n-lhs.n; return Integer(Negative,dif); }
+                    }
+                    break;
+
+                case Sign::NN:
+                    switch( Natural::Compare(lhs.n,rhs.n) )
+                    {
+                        case __Zero__: break; // ==> zero
+                        case Positive: { const Natural dif = lhs.n-rhs.n; return Integer(Negative,dif); }
+                        case Negative: { const Natural dif = rhs.n-lhs.n; return Integer(Positive,dif); }
+                    }
+                    break;
+            }
             return Integer();
         }
     }
