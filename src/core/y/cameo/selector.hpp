382b00a3fa562d346aee3c5b1efe307290e332a6
ybouret <yann.bouret@gmail.com>
2025-07-03 15:42:51 +0200

    wip

---
+++ src/core/y/cameo/selector.hpp
@@ -0,0 +1,20 @@
+
+//! \file
+
+#ifndef Y_Cameo_Selector_Included
+#define Y_Cameo_Selector_Included 1
+
+#include "y/type/traits.hpp"
+#include "y/apex/number.hpp"
+
+namespace Yttrium
+{
+
+    namespace Cameo
+    {
+        
+    }
+
+}
+
+#endif
