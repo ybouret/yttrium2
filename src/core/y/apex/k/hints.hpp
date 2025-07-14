1916ac68e4bfc9b48f4eb8947f2fa83f4e375e5a
ybouret <yann.bouret@gmail.com>
2025-07-14 12:17:00 +0200

    wip

---
+++ src/core/y/apex/k/hints.hpp
@@ -0,0 +1,23 @@
+
+
+//! \file
+
+#ifndef Y_Apex_Hints_Included
+#define Y_Apex_Hints_Included 1
+
+
+#include "y/core/setup.hpp"
+
+namespace Yttrium
+{
+    namespace Apex
+    {
+
+
+
+    }
+
+}
+
+#endif
+
