b41751bed5c768745d1e90ecd5d6b89790b7cb38
Yann Bouret <yann.bouret@unice.fr>
2025-07-16 12:29:59 +0200

    wip

--- src/core/y/apex/q/alg.cpp
+++ src/core/y/apex/q/alg.cpp
@@ -1,5 +1,7 @@
 
 #include "y/apex/rational.hpp"
+#include "y/system/exception.hpp"
+#include <cerrno>
 
 namespace Yttrium
 {
@@ -13,13 +15,34 @@ namespace Yttrium
 
         Rational Rational:: sqrt() const
         {
-            const Integer n = numer.sqrt();
-            Natural       d = denom.sqrt();
-            const Natural & N = numer.n;
-            const Natural & D = denom;
-            const Natural   threshold = D * n.n.sqr();
-            while( d.sqr() * N < threshold) ++d;
-            return Rational(n,d);
+
+            switch(numer.s)
+            {
+                case 0: return Rational();
+                case Negative: throw Libc::Exception(EDOM,"sqrt of negative %s",CallSign);
+                case Positive:
+                    break;
+            }
+
+            const Natural &D = denom;
+            const Natural &N = numer.n;
+            Natural        a = N;
+            Natural        b = D;
+
+            for(size_t i=0;i<4;++i)
+            {
+                Natural newB = b * a * D;
+                Natural newA = D*a.sqr() + N * b.sqr();
+                newA.shr();
+                Natural::Simplify(newA,newB);
+                const Rational q(Positive,newA,newB);
+                std::cerr << "s=" << q << std::endl;
+
+                a.xch(newA);
+                b.xch(newB);
+            }
+
+            return Rational();
         }
     }
 
