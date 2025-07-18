2f05ff2d93332f4a2c4c619d1bc59c2a5f91f587
ybouret <yann.bouret@gmail.com>
2025-07-18 13:38:03 +0200

    wip

--- src/mkl/y/mkl/root/zrid.hxx
+++ src/mkl/y/mkl/root/zrid.hxx
@@ -25,13 +25,27 @@ void ZRid<real_t>:: lookup(Triplet<real_t> &x, Triplet<real_t> &f, FunctionType 
     // check and organize data
     //
     //-------------------------------------------------------------------------_
-    {
+
         Handle hx = { 0,0 };
         Handle hf = { 0,0 };
 
         if( found(hx, hf, x, f, F) ) return;
-    }
 
+
+    const bool increasing = (hf.neg == &f.a);
+
+
+    {
+        f.b = F( x.b=Half<real_t>::Of(x.a+x.c) );
+        const SignType s_b = Sign::Of(f.b);
+        switch(s_b)
+        {
+            case __Zero__:
+                // exact
+                return;
+        }
+
+    }
 
 
 }
