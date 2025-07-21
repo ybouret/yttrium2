920b4735f333ce6bcff00b4d4247a5d54be79ddc
Yann Bouret <yann.bouret@unice.fr>
2025-07-21 08:55:35 +0200

    wip

--- etc/y.cmake
+++ etc/y.cmake
@@ -210,9 +210,9 @@ function(Y_LinkLibraries THE_EXE)
 	set(THE_LIBS "")
 
 	# forward libraries
+	list(APPEND THE_LIBS "y")
 	list( APPEND THE_LIBS ${ARGN} )
 
-	list(APPEND THE_LIBS "y")
 	if(Y_Linux OR Y_FreeBSD)
 		list( APPEND THE_LIBS "pthread" )
 	endif()
