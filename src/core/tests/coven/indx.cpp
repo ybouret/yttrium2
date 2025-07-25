525c5e747f671437249ff5fdc7c33bcc8901cba9
ybouret <yann.bouret@gmail.com>
2025-07-25 21:06:28 +0200

    wip

--- src/core/tests/coven/indx.cpp
+++ src/core/tests/coven/indx.cpp
@@ -11,11 +11,21 @@ Y_UTEST(coven_indx)
 
     {
         Coven::IList     il(pool);
-        il << 1 << 2 << 4 << 7;
+        il << 7 << 2 << 4 << 7;
         std::cerr << il << std::endl;
     }
 
     std::cerr << "pool.size=" << pool.count() << std::endl;
+
+    {
+        Coven::IList     il(pool);
+        Coven::ISet      is(pool);
+        il << 1 << 7 << 3 << 10;
+        std::cerr << il << std::endl;
+        while(il.size()) is << il->popHead();
+        std::cerr << is << std::endl;
+    }
+
 
 }
 Y_UDONE()
