f0f84da79f92ac6dc2a3548e1698f993133067ae
Yann Bouret <yann.bouret@unice.fr>
2025-08-01 10:43:43 +0200

    wip

--- src/core/tests/system/wall-time.cpp
+++ src/core/tests/system/wall-time.cpp
@@ -2,6 +2,7 @@
 #include "y/utest/run.hpp"
 #include <cerrno>
 #include <cmath>
+#include "y/memory/stealth.hpp"
 
 using namespace Yttrium;
 
@@ -17,15 +18,16 @@ Y_UTEST(system_wall_time)
     }
     std::cerr << "ellapsed=" << chrono.since(ini) << std::endl;
 
-    uint64_t probe = 0;
-    double   sum   = 0;
+    uint64_t   probe = 0;
+    uint64_t * ptmx  = Memory::Stealth::Cast<uint64_t>(&probe);
+    double     sum   = 0;
     for(unsigned u=0;u<50000;++u)
     {
-        Y_WallTime_Mark(&probe);
+        Y_WallTime_Mark(ptmx);
         sum += cos( double(u) );
-        Y_WallTime_Gain(&probe);
+        Y_WallTime_Gain(ptmx);
     }
-    std::cerr << "sum=" << sum << " in " << chrono(probe) << std::endl;
+    std::cerr << "sum=" << sum << " in " << chrono(probe) << "/s" << std::endl;
 
 }
 Y_UDONE()
