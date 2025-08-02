e7dee76865b44fd2f43600e8aa4bae032e19e3d1
ybouret <yann.bouret@gmail.com>
2025-08-02 16:09:57 +0200

    wip

--- src/core/tests/type/fourcc.cpp
+++ src/core/tests/type/fourcc.cpp
@@ -7,6 +7,6 @@ Y_UTEST(type_fourcc)
 {
     std::cerr << FourCC(0x12345678) << std::endl;
     std::cerr << FourCC( Y_FOURCC('a', 'b', 'c', 'd') ) << std::endl;
-
+    std::cerr << FourCC(0xffffffff) << std::endl;
 }
 Y_UDONE()
