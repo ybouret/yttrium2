ab611cac74054c6c45422f0c090df7ed6bdd50e1
ybouret <yann.bouret@gmail.com>
2025-08-11 12:42:08 +0200

    wip

--- src/jive/y/jive/pattern/first-chars.cpp
+++ src/jive/y/jive/pattern/first-chars.cpp
@@ -8,195 +8,195 @@
 
 namespace Yttrium
 {
-    namespace Jive
-    {
-        FirstChars:: FirstChars() noexcept :
-        size(0),
-        data()
-        {
-            free();
-            assert(0==Calculus::BitsIn::Block(data,sizeof(data)));
-        }
+	namespace Jive
+	{
+		FirstChars::FirstChars() noexcept :
+			size(0),
+			data()
+		{
+			free();
+			assert(0 == Calculus::BitsIn::Block(data, sizeof(data)));
+		}
 
 
-        void FirstChars:: free() noexcept
-        {
-            memset(data,0,sizeof(data));
-            Coerce(size) = 0;
-        }
+		void FirstChars::free() noexcept
+		{
+			memset(data, 0, sizeof(data));
+			Coerce(size) = 0;
+		}
 
-        void FirstChars:: all() noexcept
-        {
-            memset(data,0xff,sizeof(data));
-            Coerce(size) = 256;
-        }
-
-
-        FirstChars:: FirstChars(const FirstChars &fc) noexcept :
-        size(fc.size),
-        data()
-        {
-            memcpy(data,fc.data,sizeof(data));
-        }
-
-        FirstChars & FirstChars:: operator=(const FirstChars &fc) noexcept
-        {
-            Coerce(size) = fc.size;
-            memmove(data,fc.data, sizeof(data) );
-            return *this;
-        }
+		void FirstChars::all() noexcept
+		{
+			memset(data, 0xff, sizeof(data));
+			Coerce(size) = 256;
+		}
 
 
-        static const uint8_t fcbit[8] = {1,2,4,8,16,32,64,128};
+		FirstChars::FirstChars(const FirstChars &fc) noexcept :
+			size(fc.size),
+			data()
+		{
+			memcpy(data, fc.data, sizeof(data));
+		}
 
-        bool FirstChars:: getbit(const uint8_t b) const noexcept
-        {
-            return 0 != (data[b>>3] & fcbit[b&7]);
-        }
-
-        void FirstChars:: setbit(const uint8_t b) noexcept
-        {
-            (data[b>>3] |= fcbit[b&7]);
-        }
-
-        static const uint8_t fcmsk[8] = {255-1,255-2,255-4,255-8,255-16,255-32,255-64,255-128};
-
-
-        void FirstChars:: clrbit(const uint8_t b) noexcept
-        {
-            data[b>>3] &= fcmsk[b&7];
-        }
+		FirstChars & FirstChars:: operator=(const FirstChars &fc) noexcept
+		{
+			Coerce(size) = fc.size;
+			memmove(data, fc.data, sizeof(data));
+			return *this;
+		}
 
 
+		static const uint8_t fcbit[8] = { 1,2,4,8,16,32,64,128 };
 
-        void FirstChars:: add(const uint8_t b) noexcept
-        {
-            if(!getbit(b))
-            {
-                ++Coerce(size);
-                setbit(b);
-                assert(size==Calculus::BitsIn::Block(data,sizeof(data)));
-            }
-        }
+		bool FirstChars::getbit(const uint8_t b) const noexcept
+		{
+			return 0 != (data[b >> 3] & fcbit[b & 7]);
+		}
 
-        void FirstChars:: add(uint8_t a, uint8_t b) noexcept
-        {
-            if(a>b) Swap(a,b);
-            const unsigned A(a), B(b);
-            for(unsigned  i=A;i<=B;++i)
-            {
-                add( uint8_t(i) );
-            }
-        }
+		void FirstChars::setbit(const uint8_t b) noexcept
+		{
+			(data[b >> 3] |= fcbit[b & 7]);
+		}
 
-        void FirstChars:: sub(const uint8_t b) noexcept
-        {
-            if(getbit(b))
-            {
-                clrbit(b);
-                --Coerce(size);
-                assert(size==Calculus::BitsIn::Block(data,sizeof(data)));
-            }
-        }
+		static const uint8_t fcmsk[8] = { 255 - 1,255 - 2,255 - 4,255 - 8,255 - 16,255 - 32,255 - 64,255 - 128 };
 
 
-        void FirstChars:: sub(uint8_t a, uint8_t b) noexcept
-        {
-            if(a>b) Swap(a,b);
-            const unsigned A(a), B(b);
-            for(unsigned  i=A;i<=B;++i)
-            {
-                sub( uint8_t(i) );
-            }
-        }
-
-        FirstChars & FirstChars:: operator +=(const FirstChars &fc) noexcept
-        {
-
-            for(size_t i=0;i<sizeof(data);++i)
-            {
-                data[i] |= fc.data[i];
-            }
-            Coerce(size) = Calculus::BitsIn::Block(data,sizeof(data));
-            return *this;
-        }
-
-        FirstChars & FirstChars:: operator -=(const FirstChars &fc) noexcept
-        {
-
-            for(size_t i=0;i<sizeof(data);++i)
-            {
-                data[i] &= ~fc.data[i];
-            }
-            Coerce(size) = Calculus::BitsIn::Block(data,sizeof(data));
-            return *this;
-        }
+		void FirstChars::clrbit(const uint8_t b) noexcept
+		{
+			data[b >> 3] &= fcmsk[b & 7];
+		}
 
 
-        unsigned FirstChars:: findLowerBit(unsigned i) const noexcept
-        {
-            assert(i<256);
-            for(;i<256;++i)
-            {
-                if(getbit(i))
-                    break;
-            }
-            return i;
-        }
 
-        unsigned FirstChars:: findUpperBit(unsigned i) const noexcept
-        {
-            assert(i<256);
-            assert(getbit(i));
-            for(++i;i<256;++i)
-            {
-                if(!getbit(i))
-                    break;
-            }
-            return i-1;
-        }
+		void FirstChars::add(const uint8_t b) noexcept
+		{
+			if (!getbit(b))
+			{
+				++Coerce(size);
+				setbit(b);
+				assert(size == Calculus::BitsIn::Block(data, sizeof(data)));
+			}
+		}
 
-        void FirstChars:: run(Proc proc, void * const args) const
-        {
-            assert(proc);
-            unsigned lower = 0;
+		void FirstChars::add(uint8_t a, uint8_t b) noexcept
+		{
+			if (a > b) Swap(a, b);
+			const unsigned A(a), B(b);
+			for (unsigned i = A; i <= B; ++i)
+			{
+				add(uint8_t(i));
+			}
+		}
 
-            while(lower<256)
-            {
-                lower = findLowerBit(lower); if(lower>=256) return;
-                const unsigned upper = findUpperBit(lower);
-                proc( uint8_t(lower), uint8_t(upper), args);
-                lower = upper+1;
-            }
+		void FirstChars::sub(const uint8_t b) noexcept
+		{
+			if (getbit(b))
+			{
+				clrbit(b);
+				--Coerce(size);
+				assert(size == Calculus::BitsIn::Block(data, sizeof(data)));
+			}
+		}
 
 
-        }
+		void FirstChars::sub(uint8_t a, uint8_t b) noexcept
+		{
+			if (a > b) Swap(a, b);
+			const unsigned A(a), B(b);
+			for (unsigned i = A; i <= B; ++i)
+			{
+				sub(uint8_t(i));
+			}
+		}
 
-        void FirstChars:: Print(const uint8_t lower, const uint8_t upper, void * args)
-        {
-            assert(upper>=lower);
-            assert(args);
-            std::ostream &os = *(std::ostream *)args;
-            os << '[';
-            if(lower>=upper)
-            {
-                os << ASCII::Printable::Char[lower];
-            }
-            else
-            {
-                os <<   ASCII::Printable::Char[lower] << '-' << ASCII::Printable::Char[upper];
-            }
-            os << ']';
-        }
+		FirstChars & FirstChars:: operator +=(const FirstChars &fc) noexcept
+		{
 
-        std::ostream & operator<<(std::ostream &os, const FirstChars &fc)
-        {
-            fc.run(FirstChars::Print,&os);
-            return os;
-        }
+			for (size_t i = 0; i < sizeof(data); ++i)
+			{
+				data[i] |= fc.data[i];
+			}
+			Coerce(size) = Calculus::BitsIn::Block(data, sizeof(data));
+			return *this;
+		}
+
+		FirstChars & FirstChars:: operator -=(const FirstChars &fc) noexcept
+		{
+
+			for (size_t i = 0; i < sizeof(data); ++i)
+			{
+				data[i] &= ~fc.data[i];
+			}
+			Coerce(size) = Calculus::BitsIn::Block(data, sizeof(data));
+			return *this;
+		}
 
 
-    }
+		unsigned FirstChars::findLowerBit(unsigned i) const noexcept
+		{
+			assert(i < 256);
+			for (; i < 256; ++i)
+			{
+				if (getbit((uint8_t)i))
+					break;
+			}
+			return i;
+		}
+
+		unsigned FirstChars::findUpperBit(unsigned i) const noexcept
+		{
+			assert(i < 256);
+			assert(getbit(i));
+			for (++i; i < 256; ++i)
+			{
+				if (!getbit((uint8_t)i))
+					break;
+			}
+			return i - 1;
+		}
+
+		void FirstChars::run(Proc proc, void * const args) const
+		{
+			assert(proc);
+			unsigned lower = 0;
+
+			while (lower < 256)
+			{
+				lower = findLowerBit(lower); if (lower >= 256) return;
+				const unsigned upper = findUpperBit(lower);
+				proc(uint8_t(lower), uint8_t(upper), args);
+				lower = upper + 1;
+			}
+
+
+		}
+
+		void FirstChars::Print(const uint8_t lower, const uint8_t upper, void * args)
+		{
+			assert(upper >= lower);
+			assert(args);
+			std::ostream &os = *(std::ostream *)args;
+			os << '[';
+			if (lower >= upper)
+			{
+				os << ASCII::Printable::Char[lower];
+			}
+			else
+			{
+				os << ASCII::Printable::Char[lower] << '-' << ASCII::Printable::Char[upper];
+			}
+			os << ']';
+		}
+
+		std::ostream & operator<<(std::ostream &os, const FirstChars &fc)
+		{
+			fc.run(FirstChars::Print, &os);
+			return os;
+		}
+
+
+	}
 
 }
 
--- src/jive/y/jive/pattern/insensitive.cpp
+++ src/jive/y/jive/pattern/insensitive.cpp
@@ -6,145 +6,145 @@
 
 namespace Yttrium
 {
-    namespace Jive
-    {
+	namespace Jive
+	{
 
-        namespace
-        {
-            static inline Pattern * IgnByte( Byte * const p )
-            {
-                AutoPtr<Pattern> motif = p;
+		namespace
+		{
+			static inline Pattern * IgnByte(Byte * const p)
+			{
+				AutoPtr<Pattern> motif = p;
 
-                const uint8_t c = p->code;
+				const uint8_t c = p->code;
 
-                if( islower(c) )
-                {
-                    AutoPtr<Logic> q = new Or();
-                    q->pushTail( motif.yield() );
-                    q->pushTail( new Byte( toupper(c)) );
-                    return q.yield();
-                }
+				if (islower(c))
+				{
+					AutoPtr<Logic> q = new Or();
+					q->pushTail(motif.yield());
+					q->pushTail(new Byte((uint8_t)toupper(c)));
+					return q.yield();
+				}
 
-                if( isupper(c) )
-                {
-                    AutoPtr<Logic> q = new Or();
-                    q->pushTail( new Byte( tolower(c)) );
-                    q->pushTail( motif.yield() );
-                    return q.yield();
-                }
+				if (isupper(c))
+				{
+					AutoPtr<Logic> q = new Or();
+					q->pushTail(new Byte((uint8_t)tolower(c)));
+					q->pushTail(motif.yield());
+					return q.yield();
+				}
 
-                return motif.yield();
-            }
+				return motif.yield();
+			}
 
-            static inline void FillLogic(const uint8_t a, const uint8_t b, void * const args)
-            {
-                assert(args);
-                static_cast<Logic *>(args)->add(a,b);
-            }
+			static inline void FillLogic(const uint8_t a, const uint8_t b, void * const args)
+			{
+				assert(args);
+				static_cast<Logic *>(args)->add(a, b);
+			}
 
-            static inline Pattern * IgnLump( Lump * const p )
-            {
-                AutoPtr<Pattern> motif = p;
+			static inline Pattern * IgnLump(Lump * const p)
+			{
+				AutoPtr<Pattern> motif = p;
 
-                FirstChars fc;
-                const int  top     = int(p->upper);
-                bool       changed = false;
-                for(int c = int(p->lower);c<=top;++c)
-                {
-                    fc.add(c);
-                    if( islower(c) ) { changed=true; fc.add(toupper(c)); }
-                    if( isupper(c) ) { changed=true; fc.add(tolower(c)); }
-                }
+				FirstChars fc;
+				const int  top = int(p->upper);
+				bool       changed = false;
+				for (int c = int(p->lower); c <= top; ++c)
+				{
+					fc.add( (uint8_t)c );
+					if (islower(c)) { changed = true; fc.add((uint8_t)toupper(c)); }
+					if (isupper(c)) { changed = true; fc.add((uint8_t)tolower(c)); }
+				}
 
-                if(changed)
-                {
-                    AutoPtr<Logic> q = new Or();
-                    fc.run(FillLogic,& *q);
-                    return q.yield();
-                }
+				if (changed)
+				{
+					AutoPtr<Logic> q = new Or();
+					fc.run(FillLogic, &*q);
+					return q.yield();
+				}
 
-                return motif.yield();
-            }
+				return motif.yield();
+			}
 
 
-            static inline Pattern * IgnNot( Not * const p )
-            {
-                AutoPtr<Pattern> motif = p;
+			static inline Pattern * IgnNot(Not * const p)
+			{
+				AutoPtr<Pattern> motif = p;
 
-                const uint8_t c = p->code;
+				const uint8_t c = p->code;
 
-                if( islower(c) )
-                {
-                    AutoPtr<Logic> q = new None();
-                    q->pushTail( motif.yield() );
-                    q->pushTail( new Byte( toupper(c)) );
-                    return q.yield();
-                }
+				if (islower(c))
+				{
+					AutoPtr<Logic> q = new None();
+					q->pushTail(motif.yield());
+					q->pushTail(new Byte((uint8_t)toupper(c)));
+					return q.yield();
+				}
 
-                if( isupper(c) )
-                {
-                    AutoPtr<Logic> q = new None();
-                    q->pushTail( new Byte( tolower(c)) );
-                    q->pushTail( motif.yield() );
-                    return q.yield();
-                }
+				if (isupper(c))
+				{
+					AutoPtr<Logic> q = new None();
+					q->pushTail(new Byte((uint8_t)tolower(c)));
+					q->pushTail(motif.yield());
+					return q.yield();
+				}
 
-                return motif.yield();
-            }
+				return motif.yield();
+			}
 
-            static inline
-            Pattern * IgnLogic(Logic * const p)
-            {
-                AutoPtr<Pattern> motif = p;
+			static inline
+				Pattern * IgnLogic(Logic * const p)
+			{
+				AutoPtr<Pattern> motif = p;
 
-                {
-                    Patterns plist;
-                    while(p->size>0)
-                    {
-                        plist.pushTail( Pattern::Insensitive(p->popHead()) );
-                    }
-                    p->swapListFor(plist);
-                }
+				{
+					Patterns plist;
+					while (p->size > 0)
+					{
+						plist.pushTail(Pattern::Insensitive(p->popHead()));
+					}
+					p->swapListFor(plist);
+				}
 
-                return motif.yield();
-            }
+				return motif.yield();
+			}
 
-            Pattern * IgnJoker(Joker * const p)
-            {
-                AutoPtr<Pattern> guard = p;
+			Pattern * IgnJoker(Joker * const p)
+			{
+				AutoPtr<Pattern> guard = p;
 
-                {
-                    Motif tmp = Pattern::Insensitive( p->motif->clone() );
-                    tmp.xch( Coerce(p->motif) );
-                }
+				{
+					Motif tmp = Pattern::Insensitive(p->motif->clone());
+					tmp.xch(Coerce(p->motif));
+				}
 
-                return guard.yield();
+				return guard.yield();
 
-            }
-        }
+			}
+		}
 
-        Pattern * Pattern:: Insensitive(Pattern * const p)
-        {
-            AutoPtr<Pattern> motif = p;
+		Pattern * Pattern::Insensitive(Pattern * const p)
+		{
+			AutoPtr<Pattern> motif = p;
 
-            switch(p->uuid)
-            {
-                case Byte::UUID: return IgnByte( motif.yield()->as<Byte>() );
-                case Lump::UUID: return IgnLump( motif.yield()->as<Lump>() );
-                case Not::UUID:  return IgnNot(  motif.yield()->as<Not>() );
+			switch (p->uuid)
+			{
+			case Byte::UUID: return IgnByte(motif.yield()->as<Byte>());
+			case Lump::UUID: return IgnLump(motif.yield()->as<Lump>());
+			case Not::UUID:  return IgnNot(motif.yield()->as<Not>());
 
-                case And::  UUID: return IgnLogic( motif.yield()->as<And>() );
-                case Or::   UUID: return IgnLogic( motif.yield()->as<Or>() );
-                case None:: UUID: return IgnLogic( motif.yield()->as<None>() );
+			case And::UUID: return IgnLogic(motif.yield()->as<And>());
+			case Or::UUID: return IgnLogic(motif.yield()->as<Or>());
+			case None::UUID: return IgnLogic(motif.yield()->as<None>());
 
-                case Option:: UUID: return IgnJoker( motif.yield()->as<Option>() );
-                case Repeat:: UUID: return IgnJoker( motif.yield()->as<Repeat>() );
-                case Amount:: UUID: return IgnJoker( motif.yield()->as<Amount>() );
+			case Option::UUID: return IgnJoker(motif.yield()->as<Option>());
+			case Repeat::UUID: return IgnJoker(motif.yield()->as<Repeat>());
+			case Amount::UUID: return IgnJoker(motif.yield()->as<Amount>());
 
-            }
+			}
 
-            return motif.yield();
-        }
-    }
+			return motif.yield();
+		}
+	}
 
 }
