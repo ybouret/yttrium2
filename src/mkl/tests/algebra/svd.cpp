74c144fdb68993463772d1d6b95965304d164f05
ybouret <yann.bouret@gmail.com>
2025-08-12 09:20:36 +0200

    wip

--- src/mkl/tests/algebra/svd.cpp
+++ src/mkl/tests/algebra/svd.cpp
@@ -13,14 +13,22 @@ namespace {
     static inline
     void testSVD(Random::Bits &ran)
     {
-        for(size_t n=1;n<=1;++n)
+        SVD<T> svd;
+        for(size_t n=1;n<=2;++n)
         {
             Matrix<T> a(n,n);
             Matrix<T> u(n,n), v(n,n);
             Vector<T> w(n,0); Y_ASSERT(n==w.size());
 
             FillWith<T>::Mat(ran,a);
-            
+
+            std::cerr << "a=" << a << std::endl;
+            if(!svd.build(u, w, v, a)) {
+                std::cerr << "couldn't build SVD" << std::endl;
+            }
+            std::cerr << "u=" << u << std::endl;
+            std::cerr << "w=diagm(" << w << "]" << std::endl;
+            std::cerr << "v=" << v << std::endl;
 
         }
 
