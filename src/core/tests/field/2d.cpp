63faaea464c25b0a0ceb80c55ffc8c8f71f0d332
Yann Bouret <yann.bouret@unice.fr>
2025-08-18 09:49:14 +0200

    wip

---
+++ src/core/tests/field/2d.cpp
@@ -0,0 +1,16 @@
+
+#include "y/field/2d.hpp"
+#include "y/utest/run.hpp"
+
+using namespace Yttrium;
+
+Y_UTEST(field_2d)
+{
+#if 0
+    const Field::Format1D  fmt = new Field::Layout1D(-5,5);
+    const Field::In1D<int> f1("f1",fmt);
+
+    Y_SIZEOF(Field::In1D<int>);
+#endif
+}
+Y_UDONE()
