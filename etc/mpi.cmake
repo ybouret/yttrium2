87ab8d4474953b8800c1232f40102f441f251205
ybouret <yann.bouret@gmail.com>
2025-09-03 13:28:11 +0200

    wip

--- etc/mpi.cmake
+++ etc/mpi.cmake
@@ -1,6 +1,7 @@
 # detection of MPI settings
 
 set(MPI_FOUND OFF)
+set(MPI_LIBRARIES "")
 
 find_program(MPICC mpicc)
  
@@ -18,4 +19,31 @@ else()
     cmake_print_variables(MPI_OUT)
     string(REGEX REPLACE " +"  ";" MPI_OUT "${MPI_OUT}")
     cmake_print_variables(MPI_OUT)
+    foreach(arg IN LISTS MPI_OUT)
+        #message( STATUS "${arg}" )
+
+        if(${arg} MATCHES "-I.*")
+            #message( STATUS "include : ${arg}" )
+            string(SUBSTRING ${arg} 2 -1 arg)
+            message( STATUS "include : ${arg}" )
+            include_directories(${arg})
+        endif()
+
+         if(${arg} MATCHES "-L.*")
+            string(SUBSTRING ${arg} 2 -1 arg)
+            message( STATUS "libpath : ${arg}" )
+            link_directories(${arg})
+        endif()
+
+         if(${arg} MATCHES "-l.*")
+            string(SUBSTRING ${arg} 2 -1 arg)
+            message( STATUS "library : ${arg}" )
+            #link_directories(${arg})
+            list( APPEND MPI_LIBRARIES ${arg})
+        endif()
+
+    endforeach()
+    
+    cmake_print_variables(MPI_LIBRARIES)
+
 endif()
--- src/mpi++/tests/test-init.cpp
+++ src/mpi++/tests/test-init.cpp
@@ -1,11 +1,13 @@
 
 #include "y/utest/run.hpp"
+#include <mpi.h>
 
 using namespace Yttrium;
 
 Y_UTEST(init)
 {
-
+    MPI_Init(&argc, &argv);
+    
 }
 Y_UDONE()
 
