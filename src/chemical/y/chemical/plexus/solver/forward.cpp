8f3e8f0c7c8d7f020107e129962e51ccc4f2cbd8
ybouret <yann.bouret@gmail.com>
2025-09-05 11:43:34 +0200

    wip

--- src/chemical/y/chemical/plexus/solver/forward.cpp
+++ src/chemical/y/chemical/plexus/solver/forward.cpp
@@ -12,7 +12,21 @@ namespace Yttrium
             assert(0!=meth);
             Y_XML_Section(xml,uuid);
 
-            const xreal_t Wtmp = ((*this).*meth)(xml);
+            const xreal_t Wtmp   = ((*this).*meth)(xml);
+            const bool    accept = Wtmp < Wnew;
+
+            Y_XMLog(xml, (accept ?"[+]" : "[-]") << " W_" << uuid << " = " << Wtmp.str()  << " / " << Wnew.str());
+
+            if(accept)
+            {
+                Wnew = Wtmp;
+                Cnew.ld(Ctry);
+                std::cerr << "Check " << affinityRMS(Cnew,SubLevel).str() << std::endl;
+                if(Wnew<=zero)
+                {
+                    return true;
+                }
+            }
 
             return false;
         }
