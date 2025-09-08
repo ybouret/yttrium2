eeeeaa3ea48e6065c113d9874296675d9e75075b
Yann Bouret <yann.bouret@unice.fr>
2025-09-08 08:18:41 +0200

    wip

--- src/dll/loader.cpp
+++ src/dll/loader.cpp
@@ -10,8 +10,8 @@ Y_PROGRAM()
 {
     if(argc>1)
     {
-        const DLL dll = argv[1];
-        Proc   const    sine = dll.load<Proc>("Sine");
+        const DLL    dll = argv[1];
+        Proc   const sine = dll.load<Proc>("Sine");
         if(sine)
         {
             for(unsigned i=0;i<8;++i)
