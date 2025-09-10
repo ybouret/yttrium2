f00101a69d401df500a0ff7bc3171ec1f5bd813c
ybouret <yann.bouret@gmail.com>
2025-09-10 10:26:58 +0200

    wip

--- src/core/tests/main.cpp
+++ src/core/tests/main.cpp
@@ -37,6 +37,7 @@ Y_UTEST_DECL(256)
     Y_UTEST(system_rand);
     Y_UTEST(system_wall_time);
     Y_UTEST(system_hardware);
+    Y_UTEST(system_rtti);
 
     Y_UTEST(calculus_integer_log2);
     Y_UTEST(calculus_alignment);
