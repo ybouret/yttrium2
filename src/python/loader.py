029da71ac79cd55c76f3d495413ffac1c2af55c7
Yann Bouret <yann.bouret@unice.fr>
2025-09-15 15:45:16 +0200

    wip

--- src/python/loader.py
+++ src/python/loader.py
@@ -1,1 +1,14 @@
-import ctypes as ctclass Loader:        def __init__(self):        self.dll = ct.cdll.LoadLibrary("./typyc.so")        self.dll.sine.argtypes = [ct.c_double]        self.dll.sine.restype = ct.c_double        self.sine = self.dll.sine                app = Loader()
+""" importing ctypes """
+import ctypes as ct
+
+
+class Application:
+
+    def __init__(self):
+        self.dll = ct.cdll.LoadLibrary("./typyc.so")
+        self.dll.sine.argtypes = [ct.c_double]
+        self.dll.sine.restype = ct.c_double
+        self.sine = self.dll.sine
+
+
+app = Application()
