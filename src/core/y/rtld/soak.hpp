b6e09c033ea22f42b6a8c2643e2a210d1a9dff25
Yann Bouret <yann.bouret@unice.fr>
2025-09-15 16:18:39 +0200

    wip

--- src/core/y/rtld/soak.hpp
+++ src/core/y/rtld/soak.hpp
@@ -7,9 +7,12 @@
 #include "y/rtld/dso.h"
 #include "y/exception.hpp"
 #include <cassert>
+#include <cstring>
 
 namespace Yttrium
 {
+
+
 
     //__________________________________________________________________________
     //
@@ -23,6 +26,7 @@ namespace Yttrium
     class Soak
     {
     public:
+        static const size_t MSGLEN = 256;
 
         //! create new CLASS on DLL load
         static inline void Enter() noexcept
@@ -30,11 +34,19 @@ namespace Yttrium
             try
             {
                 assert(0==App);
+                memset(What,0,sizeof(What));
+                memset(When,0,sizeof(When));
                 App = new CLASS();
+            }
+            catch(std::exception &excp)
+            {
+                strncpy(What,excp.what(),MSGLEN);
+                strncpy(When,Core::Unknown,MSGLEN);
             }
             catch(...)
             {
-
+                strncpy(What,Core::Unknown,MSGLEN);
+                strncpy(When,Core::Unknown,MSGLEN);
             }
         }
 
@@ -48,21 +60,28 @@ namespace Yttrium
             }
         }
 
-        static CLASS * App;              //!< dll main object
 
+
+
+        static CLASS * App;              //!< dll main object
+        static char    What[MSGLEN];
+        static char    When[MSGLEN];
     private:
         Y_Disable_Copy_And_Assign(Soak); //!< discarding
 
     };
 
-    template <typename CLASS>
-    CLASS * Soak<CLASS>:: App = 0;
+    template <typename CLASS> CLASS * Soak<CLASS>:: App = 0;
+    template <typename CLASS> char    Soak<CLASS>:: What[];
+    template <typename CLASS> char    Soak<CLASS>:: When[];
 
     //! I/O for dll embedded class
 #define Y_Soak(CLASS) \
-Y_DLL_SETUP(Yttrium::Soak<CLASS>::Enter,Yttrium::Soak<CLASS>::Leave) Y_DLL_EXTERN()     \
-Y_EXPORT bool Y_DLL_API CLASS##WasInit() noexcept { return Yttrium::Soak<CLASS>::App; } \
-Y_DLL_FINISH() \
+Y_DLL_SETUP(Yttrium::Soak<CLASS>::Enter,Yttrium::Soak<CLASS>::Leave) Y_DLL_EXTERN()           \
+Y_EXPORT bool         Y_DLL_API CLASS##Init() noexcept { return Yttrium::Soak<CLASS>::App;  } \
+Y_EXPORT const char * Y_DLL_API CLASS##What() noexcept { return Yttrium::Soak<CLASS>::What; } \
+Y_EXPORT const char * Y_DLL_API CLASS##When() noexcept { return Yttrium::Soak<CLASS>::When; } \
+Y_DLL_FINISH()
 
 }
 
