2ae62ee49aa49185431fdde1356c778ee0e474db
Yann Bouret <yann.bouret@unice.fr>
2025-09-15 15:53:39 +0200

    wip

--- src/python/loader.py
+++ src/python/loader.py
@@ -1,14 +1,16 @@
 """ importing ctypes """
 import ctypes as ct
 
-
+""" Application Prototype """
 class Application:
 
     def __init__(self):
         self.dll = ct.cdll.LoadLibrary("./typyc.so")
-        self.dll.sine.argtypes = [ct.c_double]
-        self.dll.sine.restype = ct.c_double
         self.sine = self.dll.sine
+        self.sine.argtypes = [ct.c_double]
+        self.sine.restype = ct.c_double
+        self.check = self.dll.ApplicationWasInit
+        self.check.restype = ct.c_bool
 
 
 app = Application()
