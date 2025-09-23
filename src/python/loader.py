5237ccc5fdc159aae6acdb4b5563c94fdaa020b3
ybouret <yann.bouret@gmail.com>
2025-09-23 12:09:45 +0200

    wip

--- src/python/loader.py
+++ src/python/loader.py
@@ -12,7 +12,7 @@ class Application:
         self.sine.argtypes = [ct.c_double]
         self.sine.restype = ct.c_double
 
-        self.what = self.dll.ApplicationWhat;
+        self.what = self.dll.ApplicationWhat
         self.what.restype = ct.c_char_p
 
         self.was_init = self.dll.ApplicationInit
@@ -20,4 +20,10 @@ class Application:
         assert self.was_init()
         print( self.what() )
 
+        self.norm = self.dll.ApplicationNorm
+        self.norm.restype = ct.c_double
+        self.norm.argtypes = [ct.c_double,ct.c_double]
+
 app = Application()
+print( app.sine(0.1) )
+print( app.norm(3,4) )
--- src/python/typyc.cpp
+++ src/python/typyc.cpp
@@ -1,6 +1,6 @@
 #include "y/rtld/soak.hpp"
 #include "y/string.hpp"
-
+#include "y/mkl/api/pythagoras.hpp"
 #include <iostream>
 #include <cmath>
 
@@ -11,7 +11,10 @@ class Application : public Soak<Application>
 public:
     static const char * const CallSign;
 
-    
+    inline double Norm(const double x, const double y)
+    {
+        return MKL::Pythagoras(x,y);
+    }
 
 private:
     Y_Disable_Copy_And_Assign(Application);
@@ -31,6 +34,7 @@ Application:: ~Application() noexcept
 }
 
 
+
 const char * const Application :: CallSign = "Application";
 
 Y_Soak_Impl(Application)
@@ -42,6 +46,8 @@ Y_EXPORT double Y_DLL_API sine(double x) noexcept
 {
     return sin(x);
 }
+
+Y_Soak_Meth(double,Application,Norm,(double x, double y),(x,y))
 
 Y_DLL_FINISH()
 
