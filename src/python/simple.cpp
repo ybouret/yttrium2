6b62b3b7f5b2c255214d776efc8bc5eedda5e3fb
Yann Bouret <yann.bouret@unice.fr>
2025-09-26 14:19:58 +0200

    wip

--- src/python/simple.cpp
+++ src/python/simple.cpp
@@ -12,14 +12,4 @@ Y_EXPORT double Y_DLL_API sine(double x) noexcept
 
 Y_DLL_FINISH()
 
-static inline void OnEnter(void) {
-    //std::cerr << "OnEnter" << std::endl;
-}
-
-static inline void OnLeave(void)
-{
-    std::cerr << "OnLeave" << std::endl;
-}
-
-Y_DLL_SETUP(OnEnter,OnLeave)
 
--- src/python/simple.py
+++ src/python/simple.py
@@ -6,6 +6,9 @@ class Simple:
     def __init__(self):
         print("-- init --")
         self.dll = ct.cdll.LoadLibrary("./simple.dll")
-
+        self.sine = self.dll.sine
+        self.sine.argtypes = [ct.c_double]
+        self.sine.restype = ct.c_double
 
 simple = Simple()
+print( simple.sine(0.1) )
