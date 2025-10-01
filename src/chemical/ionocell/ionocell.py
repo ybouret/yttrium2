cce139f3908d9d4da43f4ced8e0ac65efcb899bb
ybouret <yann.bouret@gmail.com>
2025-10-01 15:49:48 +0200

    wip

--- src/chemical/ionocell/ionocell.py
+++ src/chemical/ionocell/ionocell.py
@@ -2,84 +2,68 @@
 import ctypes as ct
 import sys
 
-class Species:
 
-    def __init__(self,species_name,species_z):
-        self.name = species_name
-        self.z = species_z
-
-    def __copy__(self):
-        print('__copy__()')
-        return Species(self.name,self.z)
-    
-    def __str__(self):
-        return self.name
-
-""" IonoCell """
 class IonoCell:
+    """ interface to C++ code """
+
+    @staticmethod
+    def c_to_p(c_string):
+        """ C-string to python string """
+        return str(c_string, "utf-8")
+
+    @staticmethod
+    def p_to_c(p_string):
+        """  python string to C-string """
+        return bytes(p_string, "utf-8")
 
     def __init__(self):
-        self.dll  = ct.cdll.LoadLibrary("./ionocell.dll")
+        self.dll = ct.cdll.LoadLibrary("./ionocell.dll")
 
-        self.what = self.dll.IonoCell_What
-        self.what.argtypes = []
-        self.what.restype = ct.c_char_p
+        self.what_ = self.dll.IonoCell_What
+        self.what_.argtypes = []
+        self.what_.restype = ct.c_char_p
 
-        self.when = self.dll.IonoCell_When
-        self.when.argtypes = []
-        self.when.restype = ct.c_char_p
+        self.when_ = self.dll.IonoCell_When
+        self.when_.argtypes = []
+        self.when_.restype = ct.c_char_p
 
-        self.Init = self.dll.IonoCell_Init
-        self.Init.argtypes = []
-        self.Init.restype = ct.c_bool
+        self.init = self.dll.IonoCell_Init
+        self.init.argtypes = []
+        self.init.restype = ct.c_bool
 
-        self.Quit = self.dll.IonoCell_Quit
-        self.Quit.argtypes = []
+        self.quit = self.dll.IonoCell_Quit
+        self.quit.argtypes = []
 
-        if not self.Init():
-            self.mustQuit()
-
+        if not self.init():
+            self.must_quit()
 
         self.declare_ = self.dll.IonoCell_declare
         self.declare_.restype = ct.c_bool
         self.declare_.argtypes = [ct.c_char_p]
 
-        self.numSpecies = self.dll.IonoCell_numSpecies
-        self.numSpecies.restype = ct.c_size_t
-        self.numSpecies.argtypes = []
-
-        self.getSpeciesName_ = self.dll.IonoCell_getSpeciesName
-        self.getSpeciesName_.restype = ct.c_char_p
-        self.getSpeciesName_.argtypes = [ct.c_size_t]
-
-        self.getSpeciesCharge = self.dll.IonoCell_getSpeciesCharge
-        self.getSpeciesCharge.restype = ct.c_int
-        self.getSpeciesCharge.argtypes = [ct.c_size_t]
-
     def __del__(self):
-        self.Quit()
+        self.quit()
 
-    def declare(self,python_string):
-        if not self.declare_( bytes(python_string,"utf-8")):
-            self.mustQuit()
+    def what(self):
+        """ C reason to python string """
+        return self.c_to_p(self.what_())
 
-    def getSpeciesName(self,i):
-        return str( self.getSpeciesName_(i), "utf-8")
+    def when(self):
+        """ C location to python string """
+        return self.c_to_p(self.when_())
 
-    def getSpecies(self,i):
-        return Species( self.getSpeciesName(i), self.getSpeciesCharge(i))
+    def declare(self, some_code):
+        if not self.declare_(self.p_to_c(some_code)):
+            self.must_quit()
 
-    def mustQuit(self):
+    def must_quit(self):
+        """ print error and exit """
         print("Error in IonoCell")
-        print( "Reason   : ", str(self.what(), "utf-8") )
-        print( "Location : ", str(self.when(), "utf-8") )
+        print("Reason   : ", self.what())
+        print("Location : ", self.when())
         sys.exit(1)
 
 
 if __name__ == '__main__':
     chemsys = IonoCell()
     chemsys.declare('Na^+ Cl^- %acetic')
-    M = chemsys.numSpecies()
-    print("numSpecies = ", M )
-    for i in range(M):
-        print( chemsys.getSpecies(i) )
