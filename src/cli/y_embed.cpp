8d255c6a8cd1b179e9685da0ad2571174237cda8
Yann Bouret <yann.bouret@unice.fr>
2025-10-06 10:28:17 +0200

    wip

--- src/cli/y_embed.cpp
+++ src/cli/y_embed.cpp
@@ -12,19 +12,18 @@ using namespace Yttrium;
 
 Y_PROGRAM()
 {
-    if(true)
+    if(false)
     {
         OutputFile fp("256.bin");
         for(int i=0;i<256;++i)
         {
             fp.write( (char)i );
         }
-
     }
     AutoPtr<InputStream>  input  = new InputFile(  argc > 1 ? argv[1] : Y_STDIN  );
     AutoPtr<OutputStream> output = new OutputFile( argc > 2 ? argv[2] : Y_STDOUT );
 
-    const size_t width = 4;
+    const size_t width = 8;
     size_t w = 0;
     char   c = 0;
     bool   nl = false;
