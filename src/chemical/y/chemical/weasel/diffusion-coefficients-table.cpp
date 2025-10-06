79df8cf9a578b911eb4f02b6a0293bd68504b21c
ybouret <yann.bouret@gmail.com>
2025-10-06 14:46:39 +0200

    wip

--- src/chemical/y/chemical/weasel/diffusion-coefficients-table.cpp
+++ src/chemical/y/chemical/weasel/diffusion-coefficients-table.cpp
@@ -11,15 +11,33 @@ namespace Yttrium
         {
         }
 
-        Weasel:: DiffusionCoefficientsTable:: DiffusionCoefficientsTable() :
-        jv()
+        Weasel:: DiffusionCoefficientsTable:: DiffusionCoefficientsTable()
         {
+            static const char fn[] = "DiffusionCoefficients";
+            JSON::Value    jv;
             JSON::Compiler json;
             {
-                Jive::Source   source( Jive::Module::OpenData("DiffusionCoefficients",
+                Jive::Source   source( Jive::Module::OpenData(fn,
                                                               DiffusionCoefficients::JSDict,
                                                               DiffusionCoefficients::Length) );
-                json(Coerce(jv),source);
+                json(jv,source);
+            }
+
+            if( JSON::IsObject != jv.type )
+            {
+                throw Specific::Exception(fn,"JSDict is not a JSON::Object!!");
+            }
+
+            const JSON::Object &obj = jv.as<JSON::Object>();
+
+            for(JSON::Object::ConstIterator it=obj.begin();it!=obj.end();++it)
+            {
+                const JSON::Pair &pair = **it;
+                //std::cerr << pair << std::endl;
+                const String & name = pair.k;
+                if(JSON::IsNumber != pair.v.type )
+                    throw Specific::Exception(fn,"value of '%s' is not a Number", name.c_str());
+                
             }
 
         }
--- src/chemical/y/chemical/weasel/diffusion-coefficients-table.hpp
+++ src/chemical/y/chemical/weasel/diffusion-coefficients-table.hpp
@@ -3,7 +3,6 @@
 #ifndef Y_Chemical_Weasel_DiffusionCoefficientsTable_Included
 #define Y_Chemical_Weasel_DiffusionCoefficientsTable_Included 1
 
-#include "y/json/value.hpp"
 #include "y/chemical/weasel.hpp"
 
 namespace Yttrium
@@ -16,7 +15,7 @@ namespace Yttrium
             explicit DiffusionCoefficientsTable();
             virtual ~DiffusionCoefficientsTable() noexcept;
 
-            const JSON::Value jv;
+
 
         private:
             Y_Disable_Copy_And_Assign(DiffusionCoefficientsTable);
