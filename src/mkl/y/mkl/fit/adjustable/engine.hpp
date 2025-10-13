e9047bae9f46928643fca6217c70bbee51cfe22a
Yann Bouret <yann.bouret@unice.fr>
2025-10-13 15:40:00 +0200

    wip

---
+++ src/mkl/y/mkl/fit/adjustable/engine.hpp
@@ -0,0 +1,23 @@
+
+//! \file
+
+#ifndef Y_Fit_AdjustableEngine_Included
+#define Y_Fit_AdjustableEngine_Included 1
+
+#include "y/mkl/fit/adjustable/common.hpp"
+#include "y/cameo/addition.hpp"
+
+namespace Yttrium
+{
+    namespace MKL
+    {
+        namespace Fit
+        {
+            
+            
+        }
+    }
+}
+
+#endif // !Y_Fit_AdjustableEngine_Included
+
