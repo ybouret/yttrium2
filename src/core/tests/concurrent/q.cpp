14246ebd4617ee2b94c893ee02a58453746c17d2
ybouret <yann.bouret@gmail.com>
2025-11-04 15:34:59 +0100

    wip

--- src/core/tests/concurrent/q.cpp
+++ src/core/tests/concurrent/q.cpp
@@ -4,6 +4,7 @@
 #include "y/concurrent/condition.hpp"
 #include "y/concurrent/api/kernel.hpp"
 #include "y/container/cxx/series.hpp"
+#include "y/core/linked/list/raw.hpp"
 
 #include "y/utest/run.hpp"
 
@@ -12,33 +13,26 @@ namespace Yttrium
     namespace Concurrent
     {
 
-        class Leader
-        {
-        public:
-            explicit Leader();
-            virtual ~Leader() noexcept;
-
-            Mutex     mutex;
-            Condition synchronize;
-
-        private:
-            Y_Disable_Copy_And_Assign(Leader);
-        };
 
 
-        class Worker
+        class Worker : public Context
         {
         public:
 
-            Worker(Leader      &ll,
+            Worker(Mutex       &mx,
+                   Condition   &cv,
                    const size_t sz,
-                   const size_t rk);
+                   const size_t rk,
+                   size_t      &nt);
             ~Worker() noexcept;
 
-            Leader &     leader;
-            Context      context;
-            Condition    linger;
-            Thread       thread;
+            Mutex       & mutex;
+            Condition   & chief;
+            Condition     brake;
+            Worker *      next;
+            Worker *      prev;
+            size_t      & done;
+            Thread        impl;
 
         private:
             Y_Disable_Copy_And_Assign(Worker);
@@ -46,11 +40,21 @@ namespace Yttrium
             void        run() noexcept;
         };
 
-        Worker:: Worker(Leader &primary, const size_t sz, const size_t rk) :
-        leader(primary),
-        context(leader.mutex,sz,rk),
-        linger(),
-        thread(Launch,this)
+        typedef RawListOf<Worker> Workers;
+
+        Worker:: Worker(Mutex       &mx,
+                        Condition   &cv,
+                        const size_t sz,
+                        const size_t rk,
+                        size_t      &nt) :
+        Context(mx,sz,rk),
+        mutex(mx),
+        chief(cv),
+        brake(),
+        next(0),
+        prev(0),
+        done(nt),
+        impl(Launch,this)
         {
         }
 
@@ -68,15 +72,20 @@ namespace Yttrium
 
         void Worker:: run() noexcept
         {
-            //------------------------------------------------------------------
-            //
-            // Entering thread : LOCK mutex
-            //
-            //------------------------------------------------------------------
-            leader.mutex.lock();
+            // initialize thread
+            mutex.lock();
+            assert(done==rank);
 
-            // suspend on LOCKED mutex
-            linger.wait(leader.mutex);
+            // signal chief that we are ready
+            ++done;
+            chief.signal();
+
+
+            // wait on a locked mutex
+            Y_Thread_Message("ready @" << *this);
+            brake.wait(mutex);
+
+
         }
 
         class Team
@@ -86,7 +95,12 @@ namespace Yttrium
             virtual ~Team() noexcept;
 
             const size_t      size;
-            CxxSeries<Worker> worker;
+            Mutex             mutex;
+            Condition         chief;
+            Workers           waiting;
+            Workers           running;
+            CxxSeries<Worker> workers;
+            size_t            ready_;
 
         private:
             Y_Disable_Copy_And_Assign(Team);
@@ -96,9 +110,33 @@ namespace Yttrium
 
         Team:: Team(const Site &site) :
         size(site->size()),
-        worker(size)
+        mutex(),
+        chief(),
+        waiting(),
+        running(),
+        workers(size),
+        ready_(0)
         {
             try {
+
+                const size_t sz = size;
+                for(size_t rk=0,wi=1;rk<size;++rk,++wi)
+                {
+                    workers.push(mutex,chief,sz,rk,ready_);
+                    Y_Lock(mutex);
+                    if(ready_<wi)
+                        chief.wait(mutex);
+                }
+                Y_Thread_Message("all done");
+
+                {
+                    size_t wi = 1;
+                    for(const PNode *node=(**site).head;node;node=node->next,++wi)
+                    {
+                        waiting.pushTail( &workers[wi] )->impl.assign(**node);
+                    }
+                }
+
 
             }
             catch(...)
@@ -116,6 +154,7 @@ namespace Yttrium
 
         Team:: ~Team() noexcept
         {
+            quit();
         }
 
 
@@ -128,7 +167,7 @@ using namespace Yttrium;
 
 Y_UTEST(concurrent_q)
 {
-
+    Concurrent::Team team( Concurrent::Site::Default );
 }
 Y_UDONE()
 
