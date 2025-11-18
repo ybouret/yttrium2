bb15a1209218cc3dace591f3b861ee0cd8509b23
ybouret <yann.bouret@gmail.com>
2025-11-18 15:13:56 +0100

    wip

--- src/core/tests/concurrent/pipeline.cpp
+++ src/core/tests/concurrent/pipeline.cpp
@@ -1,90 +1,80 @@
-#include "y/concurrent/thread/site.hpp"
-#include "y/concurrent/api/pipeline/queue/squad.hpp"
 
 #include "y/concurrent/api/pipeline/queue.hpp"
 #include "y/concurrent/api/pipeline/alone.hpp"
 
-
+#include "y/random/park-miller.hpp"
+#include "y/container/sequence/vector.hpp"
 #include "y/utest/run.hpp"
+
+
+
 
 
 using namespace Yttrium;
 
+static Writable<size_t> * track = 0;
+
 namespace
 {
+
     class Something
     {
     public:
-
-        inline Something(const int arg) : value(arg)
-        {
-        }
-
-
-        inline ~Something() noexcept
-        {
-        }
-
-        inline Something(const Something &_) : value(_.value)
-        {
-        }
+        Something(const int a) : value(a), ran() {}
+        ~Something() noexcept {}
+        Something(const Something &_) : value(_.value), ran() {}
 
         void operator()(const Concurrent::Context &ctx)
         {
-            Y_Lock(ctx.sync);
-            (std::cerr << "exec @" << ctx << " => " << value << std::endl).flush();
-
+            {
+                Y_Lock(ctx.sync);
+                (std::cerr << value << " @" << ctx << std::endl).flush();
+            }
+            volatile double sum = 0;
+            for(size_t i = ran.in<size_t>(1000,1000000);i>0;--i)
+            {
+                sum += ran.to<double>();
+            }
+            if(track)
+            {
+                ++(*track)[ctx.indx];
+            }
         }
 
-
-
-        int value;
-
+        int                value;
+        Random::ParkMiller ran;
     private:
         Y_Disable_Assign(Something);
     };
+
 }
 
 Y_UTEST(concurrent_pipeline)
 {
-    Concurrent::Kernels klist;
-    Something           something(7);
 
-
-    std::cerr << "Create Kernels..." << std::endl;
+    Concurrent::Alone   stQ;
+    Concurrent::Queue   mtQ( Concurrent::Site::Default );
+    Concurrent::Kernels kernels;
     for(int i=1;i<=30;++i)
     {
-        something.value = i;
-        klist << something;
+        kernels << Something(i);
     }
 
-    std::cerr << "Show Kernels..." << std::endl;
-    for(Concurrent::Kernels::Iterator it=klist.begin();it!=klist.end();++it)
-    {
-        Concurrent::KernelTest::ST(*it);
-    }
+    Concurrent::TaskIDs  taskIDs;
 
-
-
-    Concurrent::TaskIDs  tids;
-    
-
-    Concurrent::Alone    stQ;
-    std::cerr << "Enqueuing/Alone" << std::endl;
-    stQ.enqueue(tids,klist);
+    std::cerr << "-- sequential -- " << std::endl;
+    stQ.enqueue(taskIDs,kernels);
     stQ.flush();
-    tids.free();
 
-    std::cerr << "Enqueuing/Queue" << std::endl;
-    Concurrent::Queue    mtQ( Concurrent::Site::Default );
-    mtQ.enqueue(tids,klist);
+    Vector<size_t> tracking(mtQ.size,0);
+    track = & tracking;
+
+    std::cerr << "-- parallel -- " << std::endl;
+    mtQ.enqueue(taskIDs,kernels);
     mtQ.flush();
 
-
-
-
+    std::cerr << "tracking=" << tracking << std::endl;
 
 }
 Y_UDONE()
-
 
