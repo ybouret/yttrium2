21d4fe131618a2deaad46ba6c6deffff56774ead
Yann Bouret <yann.bouret@unice.fr>
2025-11-26 16:01:20 +0100

    wip

---
+++ src/mpi++/y/mpi++/codec/apz.cpp
@@ -0,0 +1,57 @@
+
+
+#include "y/mpi++/api.hpp"
+#include "y/apex/integer.hpp"
+
+namespace Yttrium
+{
+
+    namespace
+    {
+        typedef int  SignT;
+        static const SignT IsNegative = -1;
+        static const SignT Is__Zero__ =  0;
+        static const SignT IsPositive =  1;
+    }
+
+    template <>
+    void MPI::Codec<apz>::Send(MPI          & mpi,
+                               const apz    & z,
+                               const size_t   target,
+                               const int      tag)
+    {
+
+        switch(z.s)
+        {
+            case __Zero__: mpi.send1(Is__Zero__,target,tag); return;
+            case Positive: mpi.send1(IsPositive,target,tag); break;
+            case Negative: mpi.send1(IsNegative,target,tag); break;
+        }
+    }
+
+    template <>
+    apz MPI::Codec<apz>::Recv(MPI &mpi, const size_t source, const int tag)
+    {
+        SignType    s  = __Zero__;
+
+        {
+            const SignT st = mpi.recv1<SignT>(source,tag);
+            switch(st)
+            {
+                case Is__Zero__: return apz();
+                case IsPositive: s = Positive; break;
+                case IsNegative: s = Negative; break;
+                default:
+                    throw Specific::Exception("MPI::Revc<apz>", "bad sign");
+            }
+        }
+
+        assert(__Zero__!=s);
+
+        return apz();
+    }
+
+
+
+
+}
