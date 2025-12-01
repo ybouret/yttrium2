cf60a3edfad584861325fe291574362b2f066285
ybouret <yann.bouret@gmail.com>
2025-12-01 13:43:21 +0100

    wip

--- src/core/tests/calculus/primes.cpp
+++ src/core/tests/calculus/primes.cpp
@@ -4,6 +4,7 @@
 #include "y/container/sequence/vector.hpp"
 #include "y/stream/libc/output.hpp"
 #include "y/calculus/bits-for.hpp"
+#include "y/format/hexadecimal.hpp"
 
 using namespace Yttrium;
 
@@ -18,6 +19,8 @@ Y_UTEST(calculus_primes)
 
     uint32_t i = 3;
     uint32_t n = 0;
+    uint32_t dmax = 0;
+
     while(true)
     {
         const uint32_t j = Prime::Next(i+1);
@@ -26,8 +29,11 @@ Y_UTEST(calculus_primes)
         const uint32_t d = (j-i); Y_ASSERT( 0 == (d&1) );
         const uint32_t delta = (d>>1) -1;
         const unsigned dbits = BitsFor(delta);
-        std::cerr << j << " -> d/2-1 = " << delta << " #bits = " << dbits << " #" << n << std::endl;
-        //if(n>=65536) break;
+        if(delta>dmax)
+        {
+            std::cerr << Hexadecimal(j) << " -> d/2-1 = " << std::setw(3) << delta << " #bits = " << std::setw(2) << dbits << " #" << std::setw(10) << n << " | " << j << std::endl;
+            dmax = delta;
+        }
 
         i=j;
 
