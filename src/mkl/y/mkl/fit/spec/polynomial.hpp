db2e9a80e20aa1258527c6ede219db8dd74f5344
Yann Bouret <yann.bouret@unice.fr>
2025-12-10 15:53:43 +0100

    wip

--- src/mkl/y/mkl/fit/spec/polynomial.hpp
+++ src/mkl/y/mkl/fit/spec/polynomial.hpp
@@ -3,7 +3,8 @@
 #ifndef Y_Fit_Polynomial_Included
 #define Y_Fit_Polynomial_Included 1
 
-#include "y/mkl/fit/adjustable.hpp"
+#include "y/mkl/fit/optimizer.hpp"
+#include "y/mkl/fit/sample.hpp"
 
 namespace Yttrium
 {
@@ -26,10 +27,8 @@ namespace Yttrium
 
                     virtual ~Coefficients() noexcept;
 
-                    Coefficients & operator<<( const size_t degree );
-                    
-
-
+                    Coefficients & operator<<(const size_t degree);
+                    size_t degreeOf(const Parameter &) const;
 
                     const String root;
 
@@ -38,6 +37,42 @@ namespace Yttrium
                     Y_Ingress_Decl();
                     Parameters parameters;
                 };
+
+                template <typename T>
+                class Model : public Coefficients
+                {
+                public:
+                    template <typename NAME>
+                    explicit Model(const NAME &name) : Coefficients(name)
+                    {
+
+                    }
+
+                    inline virtual ~Model() noexcept
+                    {
+
+                    }
+
+                    inline void operator()(Optimizer<T> &optim,
+                                           Sample<T,T>  &sample)
+                    {
+
+                        const Parameters &prms = **this;
+                        Variables &       vars = sample.vars;
+                        const size_t      nvar = prms->size();
+                        //aorg.adjust(n,0);
+                    }
+
+                    Vector<T> aorg;
+                    Vector<T> aerr;
+                    
+
+
+
+                private:
+                    Y_Disable_Copy_And_Assign(Model);
+                };
+
             };
 
         };
