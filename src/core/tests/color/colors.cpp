54269fc113f70d730c3dcc39719e6629189c974d
Yann Bouret <yann.bouret@unice.fr>
2025-12-10 08:32:39 +0100

    wip

--- src/core/tests/color/colors.cpp
+++ src/core/tests/color/colors.cpp
@@ -2,6 +2,7 @@
 #include "y/color/rgba.hpp"
 
 #include "y/utest/run.hpp"
+#include "y/format/hexadecimal.hpp"
 
 using namespace Yttrium;
 
@@ -11,6 +12,9 @@ Y_UTEST(colors)
     std::cerr << Color::Privy::OpaqueFP<double>      ::Value << std::endl;
     std::cerr << Color::Privy::OpaqueFP<long double> ::Value << std::endl;
 
+    std::cerr << (unsigned)Color::Privy::OpaqueIP<uint8_t> ::Value<< std::endl;
+    std::cerr << Color::Privy::OpaqueIP<uint16_t> ::Value << std::endl;
+    std::cerr << Color::Privy::OpaqueIP<uint32_t> ::Value << std::endl;
 }
 Y_UDONE()
 
