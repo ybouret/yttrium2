953c58e777fe53b0b7eebbdacedaee92f02c636a
ybouret <yann.bouret@gmail.com>
2025-12-16 14:21:13 +0100

    wip

--- src/mkl/tests/io/data-set.cpp
+++ src/mkl/tests/io/data-set.cpp
@@ -1,5 +1,9 @@
 
 #include "y/mkl/io/data-set.hpp"
+#include "y/container/sequence/vector.hpp"
+#include "y/container/sequence/list.hpp"
+#include "y/stream/libc/input.hpp"
+
 #include "y/utest/run.hpp"
 
 using namespace Yttrium;
@@ -7,7 +11,28 @@ using namespace Yttrium;
 Y_UTEST(io_data_set)
 {
     Y_SIZEOF(MKL::DataColumn);
-    
+
+    Vector<double> X;
+    List<float>    Y;
+    Vector<int>    Z;
+
+    {
+        MKL::DataSet ds;
+        ds.add(1,X);
+        ds.add(3,Z);
+        ds.add(2,Y);
+        if(argc>1)
+        {
+            InputFile fp(argv[1]);
+            ds.load(fp);
+        }
+    }
+
+    std::cerr << "X=" << X << std::endl;
+    std::cerr << "Y=" << Y << std::endl;
+    std::cerr << "Z=" << Z << std::endl;
+
+
 }
 Y_UDONE()
 
