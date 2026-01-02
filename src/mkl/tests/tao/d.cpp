a386860a4d5e0165c7ebeec59b8db9e6bff85fc0
Yann Bouret <yann.bouret@unice.fr>
2026-01-02 15:27:51 +0100

    wip

--- src/mkl/tests/tao/d.cpp
+++ src/mkl/tests/tao/d.cpp
@@ -14,16 +14,29 @@ Y_UTEST(tao_d)
 {
 
     const size_t n = EnvironmentConvert::To<size_t>("n",5);
-    size_t count = 0;
+    size_t       m = 0;
     for(size_t i=1;i<=n;++i)
     {
+        std::cerr << "i=" << std::setw(3) << i << ":";
         for(size_t j=i;j<=n;++j)
         {
-            ++count;
+            ++m;
+            std::cerr << " " << std::setw(3) << m;
         }
+        const size_t Ki = i * n - i*(i-1)/2;
+        std::cerr << " | [" << Ki << "]" << std::endl;
     }
 
-    std::cerr << "count = " << count << " / " << ((n*(n+1))>>1) << std::endl;
+    std::cerr << "count = " << m << " / " << ((n*(n+1))>>1) << std::endl;
+
+    return 0;
+
+    for(size_t k=1;k<=m;++k)
+    {
+        std::cerr << "k=" << std::setw(3) << k;
+        std::cerr << ' ' << (2*(k-1))/n;
+        std::cerr << std::endl;
+    }
 }
 Y_UDONE()
 
