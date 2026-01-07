e6bb52a5c1f82d5ab46e19ea29340bbd56601722
Yann Bouret <yann.bouret@unice.fr>
2026-01-07 15:35:03 +0100

    wip

--- src/core/tests/information/alphabet.cpp
+++ src/core/tests/information/alphabet.cpp
@@ -15,11 +15,71 @@ namespace Yttrium
 
         enum Category
         {
-            Streaming=1, //!< no  EOS
-            Messaging=2  //!< use EOS
+            Streaming=1, //!< +NYT, -EOS
+            Messaging=2  //!< +NYT, +EOS
+        };
+
+        class Character
+        {
+        public:
+            typedef uint16_t DataType; //!< alias for bytes + control
+            typedef uint32_t FreqType; //!< alias
+            typedef uint64_t CodeType; //!< alias
+
+            const DataType data;
+            CodeType       code;
+            unsigned       bits;
+            FreqType       freq;
+            Character *    next;
+            Character *    prev;
+
+        private:
+            Y_Disable_Copy_And_Assign(Character);
+            Character();
+            ~Character() noexcept;
         };
 
 
+        class Alphabet
+        {
+        public:
+            typedef Character::DataType DataType;
+            typedef Character::FreqType FreqType;
+            typedef Character::CodeType CodeType;
+
+            static const DataType Chars = 256;
+            static const DataType NYT   = Chars;
+            static const DataType EOS   = NYT+1;
+
+
+            explicit Alphabet(const Category);
+            virtual ~Alphabet() noexcept;
+
+            const Category    category;
+            const DataType    universe;
+            const DataType    detected;
+            const FreqType    totality;
+            Character * const character;
+            
+        private:
+            Y_Disable_Copy_And_Assign(Alphabet);
+
+
+        };
+
+        Alphabet:: Alphabet(const Category c) :
+        category(c),
+        universe(Chars+category),
+        detected(0),
+        totality(0),
+        character(0)
+        {
+
+        }
+
+        Alphabet:: ~Alphabet() noexcept
+        {
+        }
 
 
 
@@ -28,7 +88,7 @@ namespace Yttrium
 
 Y_UTEST(info_alphabet)
 {
-    
+
 }
 Y_UDONE()
 
