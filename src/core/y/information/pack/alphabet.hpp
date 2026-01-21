7fdcc8e8ad7b487f8013e210ae58014dd0f5ef39
Yann Bouret <yann.bouret@unice.fr>
2026-01-21 09:22:36 +0100

    wip

--- src/core/y/information/pack/alphabet.hpp
+++ src/core/y/information/pack/alphabet.hpp
@@ -19,36 +19,70 @@ namespace Yttrium
     {
         namespace Pack
         {
+            //__________________________________________________________________
+            //
+            //
+            //
+            //! incremental alphabet
+            //
+            //
+            //__________________________________________________________________
             class Alphabet
             {
             public:
-                typedef Character::DataType  DataType;
-                typedef Character::FreqType  FreqType;
-                typedef Character::CodeType  CodeType;
-                typedef RawListOf<Character> CharList;
+                //______________________________________________________________
+                //
+                //
+                // Definitions
+                //
+                //______________________________________________________________
+                typedef Character::DataType  DataType; //!< alias
+                typedef Character::FreqType  FreqType; //!< alias
+                typedef Character::CodeType  CodeType; //!< alias
+                typedef RawListOf<Character> CharList; //!< alias
 
-                static const DataType Chars      = 256;
-                static const DataType NYT        = Chars; //!< with 0 freq
-                static const DataType EOS        = NYT+1; //!< with 0 freq
-                static const FreqType MaxFreq    = IntegerFor<FreqType>::Maximum;
-                static const size_t   SizeOfChar = sizeof(Character);
-                static const size_t   InnerChars = 1 + EOS;
-                static const size_t   InnerBytes = InnerChars * SizeOfChar;
-                static const size_t   InnerWords = Alignment::WordsGEQ<InnerBytes>::Count;
+                static const DataType Chars      = 256;                                    //!< data cjars
+                static const DataType NYT        = Chars;                                  //!< with 0 freq
+                static const DataType EOS        = NYT+1;                                  //!< with 0 freq
+                static const FreqType MaxFreq    = IntegerFor<FreqType>::Maximum;          //!< max frequency
+                static const size_t   SizeOfChar = sizeof(Character);                      //!< alias
+                static const size_t   InnerChars = 1 + EOS;                                //!< data+cntl
+                static const size_t   InnerBytes = InnerChars * SizeOfChar;                //!< local memory
+                static const size_t   InnerWords = Alignment::WordsGEQ<InnerBytes>::Count; //!< local words
 
-                explicit Alphabet(const Category);
-                virtual ~Alphabet() noexcept;
+                //______________________________________________________________
+                //
+                //
+                // C++
+                //
+                //______________________________________________________________
+                explicit Alphabet(const Category); //!< setup for a given categoru
+                virtual ~Alphabet() noexcept;      //!< cleanup
 
+                //______________________________________________________________
+                //
+                //
+                // Methods
+                //
+                //______________________________________________________________
+
+                //! \return updated with single byte
                 Alphabet & operator<<(const uint8_t) noexcept;
 
-                void display(std::ostream &) const;
-                void reduce() noexcept;
-                void reset()  noexcept; //!< reset all
+                void display(std::ostream &) const; //!< display status
+                void reduce()             noexcept; //!< reduce entropy
+                void reset()              noexcept; //!< reset all
 
                 void emit(IO::Bits &bits, const uint8_t byte, IO::Bits &pool) const;
-                
-                const Category    category;
-                const DataType    universe;
+
+                //______________________________________________________________
+                //
+                //
+                // Members
+                //
+                //______________________________________________________________
+                const Category    category;   //!< usage
+                const DataType    universe;   //!< depending on usage
                 bool              writeNYT;   //!< monitoring need for NYT
                 DataType          detected;   //!< up to Codes to remove NYT
                 FreqType          totality;   //!< current sum of frequencies
@@ -56,15 +90,14 @@ namespace Yttrium
                 
             public:
                 Character * const database;   //!< database
-                Character * const nyt;
-                Character * const eos;
+                Character * const nyt;        //!< NYT location
+                Character * const eos;        //!< EOS location
 
             private:
-                Y_Disable_Copy_And_Assign(Alphabet);
-                void * wksp[InnerWords];
+                Y_Disable_Copy_And_Assign(Alphabet); //!< discarding
+                void * wksp[InnerWords];             //!< inner memory
 
                 void emit(IO::Bits &bits, const Character &ch, IO::Bits &pool) const;
-
                 void setup() noexcept; //!< initial setup on a clean workspace
                 void cntrl() noexcept; //!< register controls in encoding
 
