4b5a36c2d80c42ce3e5b83bd61d71c335fb62eed
Yann Bouret <yann.bouret@unice.fr>
2026-01-21 12:43:30 +0100

    wip

---
+++ src/ink/y/ink/colors.hpp
@@ -0,0 +1,19 @@
+
+
+#ifndef Y_Ink_Colors_Included
+#define Y_Ink_Colors_Included 1
+
+#include "y/color/conv.hpp"
+
+
+namespace Yttrium
+{
+    namespace Ink
+    {
+        
+    }
+
+}
+
+#endif // !Y_Ink_Colors_Included
+
