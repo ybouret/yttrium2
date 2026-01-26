7ba39600dc63157a7009891b39457c71d3d914bd
Yann Bouret <yann.bouret@unice.fr>
2026-01-26 11:22:43 +0100

    wip

--- src/ink/y/ink/ops/collect.cpp
+++ src/ink/y/ink/ops/collect.cpp
@@ -12,8 +12,12 @@ namespace Yttrium
         w( 1 + (deltaX<<1)  ),
         dy( -(unit_t)deltaY ),
         h( 1 + (deltaY<<1)  ),
-        items( w*h )
+        items( w*h ),
+        ihalf( (items-1) >> 1 )
         {
+            assert( 0 != (w&1) );
+            assert( 0 != (h&1) );
+            assert( 0 != (items&1) );
 
         }
 
--- src/ink/y/ink/ops/collect.hpp
+++ src/ink/y/ink/ops/collect.hpp
@@ -3,8 +3,9 @@
 #ifndef Y_Ink_Collect_Included
 #define Y_Ink_Collect_Included 1
 
-#include "y/ink/pixmap.hpp"
-#include "y/ink/broker.hpp"
+#include "y/ink/ops.hpp"
+#include "y/core/utils.hpp"
+#include "y/sorting/heap.hpp"
 
 namespace Yttrium
 {
@@ -18,13 +19,64 @@ namespace Yttrium
                              const size_t deltaY) noexcept;
             virtual ~Collect() noexcept;
 
+            template <typename T, typename PIXMAP> inline
+            void load(T * target, PIXMAP & source, const Coord origin)
+            {
+                unit_t y = origin.y+dy;
+                for(size_t j=h;j>0;--j)
+                {
+                    typename PIXMAP::Row & row = source[y++];
+                    unit_t x = origin.x + dx;
+                    for(size_t i=w;i>0;--i)
+                        *(target++) = row[x++];
+                }
+            }
+
+            template <typename T, typename PIXMAP> inline
+            T minimum(PIXMAP & source, const Coord origin)
+            {
+                T      res = source[origin.y][origin.x];
+                unit_t y = origin.y+dy;
+                for(size_t j=h;j>0;--j)
+                {
+                    typename PIXMAP::Row & row = source[y++];
+                    unit_t x = origin.x + dx;
+                    for(size_t i=w;i>0;--i)
+                        InSituMin(res,row[x++]);
+                }
+                return res;
+            }
+
+            template <typename T, typename PIXMAP> inline
+            T maximum(PIXMAP & source, const Coord origin)
+            {
+                T      res = source[origin.y][origin.x];
+                unit_t y = origin.y+dy;
+                for(size_t j=h;j>0;--j)
+                {
+                    typename PIXMAP::Row & row = source[y++];
+                    unit_t x = origin.x + dx;
+                    for(size_t i=w;i>0;--i)
+                        InSituMax(res,row[x++]);
+                }
+                return res;
+            }
+
+            template <typename T, typename PIXMAP> inline
+            T median(T * const target, PIXMAP & source, const Coord origin)
+            {
+                load(target,source,origin);
+                Sorting::Heap::Sort(target,items,Sign::Increasing<T>);
+                return target[ihalf];
+            }
 
             const unit_t dx;
             const size_t w;
             const unit_t dy;
             const size_t h;
             const size_t items;
-
+            const size_t ihalf;
+            
         private:
             Y_Disable_Copy_And_Assign(Collect);
         };
