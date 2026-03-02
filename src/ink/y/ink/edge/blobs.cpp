9061a6e4b9c024a7c445e8463d963a1ac8ee32be
ybouret <yann.bouret@gmail.com>
2026-03-02 15:51:17 +0100

    wip

--- src/ink/y/ink/edge/blobs.cpp
+++ src/ink/y/ink/edge/blobs.cpp
@@ -1,5 +1,6 @@
 
 #include "y/ink/edge/blobs.hpp"
+#include "y/ink/ops/ld.hpp"
 
 namespace Yttrium
 {
@@ -18,6 +19,62 @@ namespace Yttrium
             { -1, -1 },
             {  1, -1 }
         };
+
+        Blobs:: Blobs(const size_t W, const size_t H) :
+        IndexMap(W,H)
+        {
+        }
+
+        Blobs:: ~Blobs() noexcept
+        {
+        }
+
+        void Blobs:: build(Broker          & broker,
+                           Pixmap<uint8_t> & edge)
+        {
+            static const Point * const delta = Memory::Stealth::Cast<Point>( &Delta[0][0] );
+            const size_t ncof = 8;
+            IndexMap &   self = *this;
+            assert(Ops::HaveSameArea(self,edge));
+
+            // initialize map
+            LoadPixel::Set(broker,*this,Zero);
+            size_t indx = 0;
+
+            // walk all the source
+            for(unit_t y=lower.y;y<=upper.y;++y)
+            {
+                const Pixmap<uint8_t>::Row & erow = edge[y];
+                IndexMap::Row              & irow = (*this)[y];
+                for(unit_t x=lower.x;x<=upper.x;++x)
+                {
+                    const uint8_t eval = erow[x];
+                    if(eval<=0)   continue; // no edge
+                    if(irow[x]>0) continue; // already detected
+
+                    // start new blob with new index
+                    ++indx; stack.free();
+                    stack << Point(x,y);
+                    while( stack.size() > 0 )
+                    {
+                        const Point p = stack.pullTail();
+                        if(0!=self[p]) { assert(indx==self[p]); continue; }
+                        self[p] = indx;
+                        for(size_t i=0;i<ncof;++i)
+                        {
+                            const Point q = p + delta[i];
+                            if(!self.contains(q)) continue;
+                            if(0==edge[q])        continue;
+                            if(0!=self[q])        continue;
+                            stack << q;
+                        }
+                    }
+                }
+            }
+
+            std::cerr << "indx=" << indx << std::endl;
+
+        }
     }
 
 }
--- src/ink/y/ink/edge/blobs.hpp
+++ src/ink/y/ink/edge/blobs.hpp
@@ -6,7 +6,7 @@
 
 
 #include "y/ink/pixmap.hpp"
-#include "y/ink/ops/ld.hpp"
+#include "y/ink/broker.hpp"
 #include "y/container/sequence/vector.hpp"
 
 namespace Yttrium
@@ -22,60 +22,12 @@ namespace Yttrium
             static const unit_t Delta[8][2];
 
 
-            explicit Blobs(const size_t W, const size_t H) :
-            IndexMap(W,H)
-            {
-            }
-
-            virtual ~Blobs() noexcept {}
+            explicit Blobs(const size_t W, const size_t H);
+            virtual ~Blobs() noexcept;
 
 
             void build(Broker          & broker,
-                       Pixmap<uint8_t> & edge)
-            {
-                static const Point * const delta = Memory::Stealth::Cast<Point>( &Delta[0][0] );
-                const size_t ncof = 8;
-                IndexMap &   self = *this;
-                assert(Ops::HaveSameArea(self,edge));
-
-                // initialize map
-                LoadPixel::Set(broker,*this,Zero);
-                size_t indx = 0;
-
-                // walk all the source
-                for(unit_t y=lower.y;y<=upper.y;++y)
-                {
-                    const Pixmap<uint8_t>::Row & erow = edge[y];
-                    IndexMap::Row              & irow = (*this)[y];
-                    for(unit_t x=lower.x;x<=upper.x;++x)
-                    {
-                        const uint8_t eval = erow[x];
-                        if(eval<=0)   continue; // no edge
-                        if(irow[x]>0) continue; // already detected
-
-                        // start new blob
-                        ++indx; stack.free();
-                        stack << Point(x,y);
-                        while( stack.size() > 0 )
-                        {
-                            const Point p = stack.pullTail();
-                            if(0!=self[p]) { assert(indx==self[p]); continue; }
-                            self[p] = indx;
-                            for(size_t i=0;i<ncof;++i)
-                            {
-                                const Point q = p + delta[i];
-                                if(!self.contains(q)) continue;
-                                if(0==edge[q])        continue;
-                                if(0!=self[q])        continue;
-                                stack << q;
-                            }
-                        }
-                    }
-                }
-
-                std::cerr << "indx=" << indx << std::endl;
-
-            }
+                       Pixmap<uint8_t> & edge);
 
             Vector<Point> stack;
 
